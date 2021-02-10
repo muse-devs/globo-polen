bb2b454169c3e9b543d2041a61529cb1cb6e661e
Ricardo Rodrigues <raorodrigues@gmail.com>
2021-02-09 22:23:18 -0300

    carregando bootstrap css e js

--- wordpress/polen/themes/polen/functions.php
+++ wordpress/polen/themes/polen/functions.php
@@ -12,6 +12,8 @@ if ( ! defined( '_S_VERSION' ) ) {
 	define( '_S_VERSION', '1.0.0' );
 }
 
+define('TEMPLATE_URI', get_template_directory_uri());
+
 if ( ! function_exists( 'polen_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
@@ -143,7 +145,11 @@ function polen_scripts() {
 	wp_enqueue_style( 'polen-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'polen-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'polen-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_style('bootstrap', TEMPLATE_URI . '/assets/bootstrap-4.6.0/dist/css/bootstrap.min.css', array(), _S_VERSION);
+	wp_enqueue_style('polen-custom-styles', TEMPLATE_URI . '/assets/css/style.css', array('bootstrap'), _S_VERSION);
+
+	wp_enqueue_script( 'polen-bootstrap-bundle', TEMPLATE_URI . '/assets/bootstrap-4.6.0/dist/js/bootstrap.bundle.min.js', array(), '4.6.0', true );
+	wp_enqueue_script( 'polen-navigation', TEMPLATE_URI . '/assets/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
