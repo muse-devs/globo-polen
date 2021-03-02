f41f7c549801f5357f9a20392e874b576434a252
Rodolfo Neto <rodolfoneto@gmail.com>
2021-03-02 12:52:15 -0300

    feat edit: removendo a admin_bar do front pela opção do Redux

--- wordpress/polen/plugins/polen_2/includes/Polen_DisableAdminBar.php
+++ wordpress/polen/plugins/polen_2/includes/Polen_DisableAdminBar.php
@@ -2,11 +2,13 @@
 
 namespace Polen\Includes;
 
-class Polen_DisableAdminBar
-{
-    function __construct()
-    {
-        show_admin_bar(false);
+class Polen_DisableAdminBar {
+
+    function __construct() {
+        $Polen_Plugin_Settings = get_option('Polen_Plugin_Settings');
+        if (isset($Polen_Plugin_Settings['admin_bar']) && strval($Polen_Plugin_Settings['admin_bar']) == strval('1')) {
+            add_filter('show_admin_bar', '__return_false');
+        }
     }
 
 }
