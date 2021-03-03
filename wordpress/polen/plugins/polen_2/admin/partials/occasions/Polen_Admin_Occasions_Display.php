c8546f50f6a65f9fc3e95df0f7d40f2b2efd917e
Rodolfo Neto <rodolfoneto@gmail.com>
2021-03-03 01:34:57 -0300

    feat edit: occasion-list usando o WP_List_Table

--- wordpress/polen/plugins/polen_2/admin/partials/occasions/Polen_Admin_Occasions_Display.php
+++ wordpress/polen/plugins/polen_2/admin/partials/occasions/Polen_Admin_Occasions_Display.php
@@ -8,17 +8,29 @@ class Polen_Admin_Occasions_Display extends \WP_List_Table
 
     public function prepare_items()
     {
+        $per_page = $this->get_items_per_page( 'occasion_per_page' );
+        $current_page = $this->get_pagenum();
+        
         $search = trim( filter_input( INPUT_GET, 's' ) );
         $occasion_list_repository = new Polen_Occasion_List();
-        $occasion_list = $occasion_list_repository->get_occasion( $search, $this->get_orderby(), $this->get_order() );
-        $this->items = $occasion_list;
+        $occasion_list = $occasion_list_repository->get_occasion( $search, $this->get_orderby(), $this->get_order(), $current_page, $per_page );
+
+        $total_items = $occasion_list_repository->get_occasion_count( $search );
+//        
+        $this->set_pagination_args([
+            'total_items' => $total_items,
+            'per_page' => $per_page
+        ]);
         
         $this->_column_headers = array( 
             $this->get_columns(),           // columns
-            array(),                        // hidden
+            $this->get_hiden_column(),      // hidden
             $this->get_sortable_columns(),  // sortable
         );
+        
+        $this->items = $occasion_list;
     }
+    
     
     public function get_columns()
     {
@@ -28,27 +40,12 @@ class Polen_Admin_Occasions_Display extends \WP_List_Table
         ];
     }
     
-    private function get_orderby()
+    
+    public function get_hiden_column()
     {
-        $orderby = trim( filter_input( INPUT_GET, 'orderby' ) );
-        return 'type';
+        return [ 'criated_at' => 'Dt Criação', 'updated_at' => 'Dt Atualização' ];
     }
     
-    private function get_order()
-    {
-        $order = trim( filter_input( INPUT_GET, 'order' ) );
-        return $order;
-    }
-    
-    public function column_type($param)
-    {
-        return $param->type;
-    }
-    
-    public function column_description($param)
-    {
-        return $param->description;
-    }
     
     public function get_sortable_columns()
     {
@@ -57,23 +54,59 @@ class Polen_Admin_Occasions_Display extends \WP_List_Table
         ];
     }
     
+    
+    private function get_orderby()
+    {
+        $orderby = trim( filter_input( INPUT_GET, 'orderby' ) );
+        return $orderby;
+    }
+    
+    
+    private function get_order()
+    {
+        $order = trim( filter_input( INPUT_GET, 'order' ) );
+        return $order;
+    }
+    
+    
+    public function column_type( $item )
+    {
+
+        $page = filter_input( INPUT_GET, 'page' );
+        $actions = [
+            'delete' => sprintf('<a href="?page=%s&action=%s&id=%s&_wpnonce=%s">', $page, 'delete', $item->id, '3123123')
+        ];
+        return sprintf( '%1$s %2$s', $item->type, $this->row_actions( $actions, true ) );
+    }
+    
+    
+    public function column_description($param)
+    {
+        return $param->description;
+    }
+    
 
     /**
      * Mostra o form de adicao de uma Occasion
      */
     public function show_form_create_occasion()
     { ?>
-        <form action="" method="post">
-            <?php wp_nonce_field('occasion_new', '_wpnonce', true, true); ?>
-            <table class="wp-list-table widefat fixed striped table-view-list toplevel_page_occasion-list">
-                <tr>
-                    <td>Categoria <input type="text" name="occasion_category" value="" required></td>
-                    <td>Descrição <input type="text" name="occasion_description" value="" required></td>
-                    <td><input type="submit" value="cadastrar" class="button-primary"></td>
-                </tr> 
-            </table>
-        </form>
-        <div>&nbsp;</div>
+        <div class="form-wrap">
+            <form class="validate" method="post" action="">
+                <?php wp_nonce_field('occasion_new', '_wpnonce', true, true); ?>
+                <div class="form-field form-required term-name-wrap">
+                    <label for="occasion_category">Categoria</label>
+                    <input name="occasion_category" id="occasion_category" type="text" value="" size="45" aria-required="true">
+                    <p>Nome da ocasião.</p>
+                </div>
+                <div class="form-field term-description-wrap">
+                        <label for="occasion_description">Descrição</label>
+                        <input name="occasion_description" id="occasion_description" type="text" value="" size="45" aria-required="true"/>
+                        <p>Texto de inspiração para usuário escolher e alterar na hora da compra.</p>
+                </div>
+                <?php submit_button( 'Cadastrar', 'primary', 'submit', true, array( 'id' => 'submit-add-occasion' ) ); ?>
+            </form>
+        </div>
     <?php
     }
     
@@ -94,4 +127,21 @@ class Polen_Admin_Occasions_Display extends \WP_List_Table
     }
     
     
+    /**
+     * Sends required variables to JavaScript land.
+     *
+     * @since 3.1.0
+     */
+//    public function _js_vars() {
+//        $args = array(
+//            'class' => get_class($this),
+//            'screen' => array(
+//                'id' => $this->screen->id,
+//                'base' => $this->screen->base,
+//            ),
+//        );
+//
+//        printf("<script type='text/javascript'>list_args = %s;</script>\n", wp_json_encode($args));
+//    }
+
 }
