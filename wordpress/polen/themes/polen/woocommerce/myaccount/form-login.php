dbf34e787ff0d24751c81c649fa36f066f9397d6
Ricardo Rodrigues <raorodrigues@gmail.com>
2021-03-13 18:28:24 -0300

    bkp

--- wordpress/polen/themes/polen/woocommerce/myaccount/form-login.php
+++ wordpress/polen/themes/polen/woocommerce/myaccount/form-login.php
@@ -28,6 +28,21 @@ do_action( 'woocommerce_before_customer_login_form' ); ?>
 	<div class="u-column1 col-1">
 
 <?php endif; ?>
+<div class="row my-5 py-5 justify-content-md-center talent-login">
+        <div class="col-12 text-center">
+            <h2 class="mb-5">Sou um talento</h2>
+        </div>
+        <div class="col-4">
+            <form action="." method="post" class="text-center">
+                <p><input type="mail" name="mail" id="mail" placeholder="e-mail" class="form-control form-control-lg" /></p>
+                <p><input type="password" name="password" id="password" placeholder="Senha" class="form-control form-control-lg" /></p>
+                <input type="submit" class="btn btn-primary btn-lg btn-block" value="Entrar" />
+                <p class="my-4"><a href="#">Esqueci minha senha</a></p>
+            </form>
+        </div>
+    </div>
+
+    <?php polen_front_get_tutorial(); ?>
 
 		<h2><?php esc_html_e( 'Login', 'woocommerce' ); ?></h2>
 
