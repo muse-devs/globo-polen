d1b5a17ff8d6adb32de96fa91b224716f41d5c8c
Rodolfo Neto <rodolfoneto@gmail.com>
2021-03-17 18:05:03 -0300

    feat edit: quando estiver em ambiente de debug apresentar a barra de admin para plugin de debug (qtd querys e uso de actions)

--- wordpress/polen/plugins/polen_2/includes/Polen_DisableAdminBar.php
+++ wordpress/polen/plugins/polen_2/includes/Polen_DisableAdminBar.php
@@ -6,7 +6,11 @@ class Polen_DisableAdminBar {
 
     function __construct() {
         $Polen_Plugin_Settings = get_option('Polen_Plugin_Settings');
-        if (isset($Polen_Plugin_Settings['admin_bar']) && strval($Polen_Plugin_Settings['admin_bar']) == strval('1')) {
+        if (
+                isset($Polen_Plugin_Settings['admin_bar']) 
+                && strval($Polen_Plugin_Settings['admin_bar']) == strval('1')
+                && !WP_DEBUG
+            ) {
             add_filter('show_admin_bar', '__return_false');
         }
     }
