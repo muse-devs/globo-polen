faeb55828e016537c4568fca6daac564aaa7c4f0
Ricardo Rodrigues <raorodrigues@gmail.com>
2021-03-21 19:11:51 -0300

    nova lista de pedidos

--- wordpress/polen/themes/polen/woocommerce/myaccount/orders-talent.php
+++ wordpress/polen/themes/polen/woocommerce/myaccount/orders-talent.php
@@ -14,7 +14,7 @@ if (in_array('user_talent',  $logged_user->roles)) {
 ?>
 <section>
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e('Suas Solicitações', 'polen'); ?></h1>
+		<h1 class="page-title"><?php esc_html_e('Meus Pedidos', 'polen'); ?></h1>
 	</header><!-- .page-header -->
 	<div class="page-content">
 		<?php
@@ -23,60 +23,51 @@ if (in_array('user_talent',  $logged_user->roles)) {
 		} else {
 			echo "<p class='mb-5'>Você tem <strong>" . count($talent_orders) . " pedidos de vídeo</strong>, seus pedidos expiram em até 7 dias.</p>";
 			if (count($talent_orders) > 0) {
-				foreach ($talent_orders as $order) :
-		?>
-					<div class="container mb-5">
-						<div class="row">
-							<div class="col-md-12">
-								<div class="talent-orders">
-									<header class="row d-flex align-items-center header">
-										<div class="col-md-4">
-											<p class="p">Valor</p>
-											<span class="value">R$200</span>
-										</div>
-										<div class="col-md-4 text-center">
-											<p class="p small">Tempo estimado</p>
-											<span class="time">2 minutos</span>
-										</div>
-										<div class="col-md-4 text-center">
-											<p class="p small">Válido por</p>
-											<span class="time">7 dias</span>
-										</div>
-									</header>
-									<div class="body">
-										<div class="row d-flex align-items-center">
-											<div class="col">
-												<p class="p small">Vídeo de</p>
-												<span class="name"><?php echo $order['from']; ?></span>
+				foreach ($talent_orders as $order) : ?>
+					<div class="row mb-5">
+						<div class="col md-12">
+							<div class="talent-order">
+								<div class="row mb-4 pb-3 bordered">
+									<div class="col-md-8">
+										<div class="row">
+											<div class="col-md-3">
+												<p class="title">Vídeo de</p>
+												<p class="description">Diego J.</p>
 											</div>
-											<div class="col text-center">
-												<?php polen_icon_arrows(); ?>
+											<div class="col-md-3"><?php polen_icon_arrows(); ?></div>
+											<div class="col-md-3">
+												<p class="title">Para</p>
+												<p class="description">Aline A.</p>
 											</div>
-											<div class="col">
-												<div>
-													<p class="p small">Para</p>
-													<span class="name"><?php echo $order['name']; ?></span>
-												</div>
+											<div class="col-md-3">
+												<p class="title">Ocasião</p>
+												<p class="description">Aniversário</p>
 											</div>
 										</div>
-										<div class="row mt-4">
-											<div class="col">
-												<p class="p small mb-3">Ocasião</p>
-												<span class="category"><?php echo $order['category']; ?></span>
+									</div>
+									<div class="col-md-4 text-right">
+										<button class="btn btn-primary btn-sm">Visualizar</button>
+										<!-- <button class="btn btn-outline-light btn-sm">Visualizar</button> -->
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-7">
+										<div class="row">
+											<div class="col-md-4">
+												<p class="title">Valor</p>
+												<p class="description">R$200</p>
 											</div>
-										</div>
-										<div class="row mt-4">
-											<div class="col">
-												<p class="p small mb-2">Instruções</p>
-												<p class="text"><?php echo $order['instructions']; ?></p>
+											<div class="col-md-4">
+												<p class="title">Tempo estimado</p>
+												<p class="description">2 min</p>
+											</div>
+											<div class="col-md-4">
+												<p class="title">Válido por</p>
+												<p class="description">24h</p>
 											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-							<div class="col-md-12 d-flex justify-content-center my-5">
-								<button class="icon-button reject mx-3"><?php polen_icon_accept_reject('reject'); ?></button>
-								<button class="icon-button accept mx-3"><?php polen_icon_accept_reject(); ?></button>
 							</div>
 						</div>
 					</div>
@@ -85,11 +76,61 @@ if (in_array('user_talent',  $logged_user->roles)) {
 				endforeach;
 			}
 		}
-
-
-
 		?>
 
+		<div class="row" style="display: none;">
+			<div class="col-md-12">
+				<div class="talent-order-modal">
+					<header class="row d-flex align-items-center header">
+						<div class="col-md-4">
+							<p class="p">Valor</p>
+							<span class="value">R$200</span>
+						</div>
+						<div class="col-md-4 text-center">
+							<p class="p small">Tempo estimado</p>
+							<span class="time">2 minutos</span>
+						</div>
+						<div class="col-md-4 text-center">
+							<p class="p small">Válido por</p>
+							<span class="time">7 dias</span>
+						</div>
+					</header>
+					<div class="body">
+						<div class="row d-flex align-items-center">
+							<div class="col">
+								<p class="p small">Vídeo de</p>
+								<span class="name"><?php echo $order['from']; ?></span>
+							</div>
+							<div class="col text-center">
+								<?php polen_icon_arrows(); ?>
+							</div>
+							<div class="col">
+								<div>
+									<p class="p small">Para</p>
+									<span class="name"><?php echo $order['name']; ?></span>
+								</div>
+							</div>
+						</div>
+						<div class="row mt-4">
+							<div class="col">
+								<p class="p small mb-3">Ocasião</p>
+								<span class="category"><?php echo $order['category']; ?></span>
+							</div>
+						</div>
+						<div class="row mt-4">
+							<div class="col">
+								<p class="p small mb-2">Instruções</p>
+								<p class="text"><?php echo $order['instructions']; ?></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-12 d-flex justify-content-center my-5">
+				<button class="icon-button reject mx-3"><?php polen_icon_accept_reject('reject'); ?></button>
+				<button class="icon-button accept mx-3"><?php polen_icon_accept_reject(); ?></button>
+			</div>
+		</div>
 
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
