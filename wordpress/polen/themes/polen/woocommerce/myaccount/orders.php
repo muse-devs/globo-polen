166ab25989a12bb53deee7dfca954125533b80fd
Ricardo Rodrigues <raorodrigues@gmail.com>
2021-04-04 20:57:17 -0300

    meus pedidos titulo

--- wordpress/polen/themes/polen/woocommerce/myaccount/orders.php
+++ wordpress/polen/themes/polen/woocommerce/myaccount/orders.php
@@ -9,6 +9,12 @@ if (in_array('user_talent',  $logged_user->roles)) {
 
 	<?php if ($has_orders) : ?>
 
+		<div class="row mt-3">
+			<div class="col-12">
+				<h1>Meus pedidos</h1>
+			</div>
+		</div>
+
 		<?php
 		foreach ($customer_orders->orders as $customer_order) {
 			$order      = wc_get_order($customer_order); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
@@ -46,13 +52,7 @@ if (in_array('user_talent',  $logged_user->roles)) {
 											}
 										?>
 										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-12 col-md-4 mt-2">
-								<div class="row">
-									<div class="col-md-12 text-center">
-										<span class="status">
+										<div class="status mt-2">
 											<?php
 											switch ($order->get_status()) {
 												case 'on-hold':
@@ -63,13 +63,13 @@ if (in_array('user_talent',  $logged_user->roles)) {
 													break;
 											}
 											?>
-										</span>
+										</div>
 									</div>
 								</div>
 							</div>
-							<div class="col-12 text-center my-3">
+							<div class="col-12 text-center mt-4 mb-3">
 								<a href="<?php echo $order->get_view_order_url(); ?>" class="btn btn-primary btn-lg btn-block">
-									Ver mais
+									Acompanhar pedido
 								</a>
 							</div>
 							<div class="col-12 text-center">
