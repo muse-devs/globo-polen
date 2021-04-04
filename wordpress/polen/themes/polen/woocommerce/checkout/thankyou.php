e8283735e755541e7d0cb16e63b44e8ec63a9eb8
Ricardo Rodrigues <raorodrigues@gmail.com>
2021-04-04 19:56:30 -0300

    thankyou básica

--- wordpress/polen/themes/polen/woocommerce/checkout/thankyou.php
+++ wordpress/polen/themes/polen/woocommerce/checkout/thankyou.php
@@ -1,36 +1,41 @@
 <?php
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 ?>
 
 <div class="woocommerce-order">
 
 	<?php
-	if ( $order ) :
+	if ($order) :
 
-		do_action( 'woocommerce_before_thankyou', $order->get_id() );
-		?>
+		do_action('woocommerce_before_thankyou', $order->get_id());
+	?>
 
-		<?php if ( $order->has_status( 'failed' ) ) : ?>
+		<?php if ($order->has_status('failed')) : ?>
 
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
+			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e('Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce'); ?></p>
 
 			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
-				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
-				<?php if ( is_user_logged_in() ) : ?>
-					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
+				<a href="<?php echo esc_url($order->get_checkout_payment_url()); ?>" class="button pay"><?php esc_html_e('Pay', 'woocommerce'); ?></a>
+				<?php if (is_user_logged_in()) : ?>
+					<a href="<?php echo esc_url(wc_get_page_permalink('myaccount')); ?>" class="button pay"><?php esc_html_e('My account', 'woocommerce'); ?></a>
 				<?php endif; ?>
 			</p>
 
 		<?php else : ?>
-			<h1>Pedido feito com sucesso</h1>
-			<h3>Seu número de pedido é <?php echo $order->get_order_number();?></h3>
-			<a href="<?php echo home_url(); ?>">Voltar para a home</a> 		
+			<div class="row mt-4">
+				<div class="col-12 text-center">
+					<h1>Pedido feito com sucesso</h1>
+					<p class="mt-2">Seu número de pedido é <?php echo $order->get_order_number(); ?></p>
+					<a href="<?php echo home_url(); ?>" class="btn btn-primary btn-lg btn-block mt-4">Voltar para a home</a>
+				</div>
+			</div>
 
 		<?php endif; ?>
 
 	<?php else : ?>
 
-		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), null ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters('woocommerce_thankyou_order_received_text', esc_html__('Thank you. Your order has been received.', 'woocommerce'), null); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+																										?></p>
 
 	<?php endif; ?>
 
