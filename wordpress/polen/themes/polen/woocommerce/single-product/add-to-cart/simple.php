5b5fc8e99ad14f64995a1a16d8d7e1fae1eab72d
Ricardo Rodrigues <raorodrigues@gmail.com>
2021-04-06 18:20:10 -0300

    talent page

--- wordpress/polen/themes/polen/woocommerce/single-product/add-to-cart/simple.php
+++ wordpress/polen/themes/polen/woocommerce/single-product/add-to-cart/simple.php
@@ -46,9 +46,9 @@ if ( $product->is_in_stock() ) : ?>
 		do_action( 'woocommerce_after_add_to_cart_quantity' );
 		?>
 
-		<button type="submit" 
-				name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" 
-				class="single_add_to_cart_button alt btn btn-primary btn-lg btn-block btn-get-video">
+		<button type="submit"
+				name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>"
+				class="single_add_to_cart_button alt btn btn-primary btn-lg btn-block btn-get-video py-3">
 				<?php echo esc_html( $product->single_add_to_cart_text() ); ?>
 		</button>
 
