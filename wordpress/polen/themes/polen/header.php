b832d6b51166c85758c0d1ee5b88040ae38deb70
Ricardo Rodrigues <raorodrigues@gmail.com>
2021-04-07 20:41:04 -0300

    sem translucent

--- wordpress/polen/themes/polen/header.php
+++ wordpress/polen/themes/polen/header.php
@@ -19,7 +19,7 @@
 	<meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+	<!-- <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> -->
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 	<script>
