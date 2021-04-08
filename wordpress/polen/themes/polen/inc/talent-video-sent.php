55399e592ed4c6740f5832c6e30d0703c203c30b
Rodolfo Neto <rodolfoneto@gmail.com>
2021-04-08 11:06:29 -0300

    ---

--- wordpress/polen/themes/polen/inc/talent-video-sent.php
+++ wordpress/polen/themes/polen/inc/talent-video-sent.php
@@ -8,7 +8,7 @@
 		<div class="col-12 text-center">
 			<img src="<?php echo TEMPLATE_URI; ?>/assets/img/cup.png" alt="">
 			<p class="mt-5">Pronto! Seu vídeo foi<br />enviado. Você ganhou:</p>
-			<p style="font-size: 44px; font-weight: 700;">R$200</p>
+			<p style="font-size: 44px; font-weight: 700;">R$500</p>
 		</div>
 		<div class="col-12 text-center">
 			<a href="/my-account/orders/" class="btn btn-outline-light btn-lg">Ver mais pedidos</a>
