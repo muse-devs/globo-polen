348e8d07848a2accebe457e49b761ee240cab379
Rodolfo Neto <rodolfoneto@gmail.com>
2021-04-08 10:42:09 -0300

    feat add: add meta data no header para apple full screen

--- wordpress/polen/themes/polen/header.php
+++ wordpress/polen/themes/polen/header.php
@@ -19,6 +19,8 @@
 	<meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-title" content="Muse">
+	<meta name="apple-touch-fullscreen" content="yes">
 	<!-- <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> -->
 	<meta name="theme-color" content="#000000">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
