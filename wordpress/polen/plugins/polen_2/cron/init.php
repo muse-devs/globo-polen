d97b71db354d23bbde7a5be088636b8cbe2346ca
Muse Dev Team <muse@c9t.pw>
2021-04-20 19:13:29 -0300

    Correção do PATH

--- wordpress/polen/plugins/polen_2/cron/init.php
+++ wordpress/polen/plugins/polen_2/cron/init.php
@@ -6,7 +6,20 @@ if( strpos(php_sapi_name(), 'cli' ) === false ) {
     die;
 }
 
+function findWordpressBasePath() {
+	$dir = dirname(__FILE__);
+	do {
+		if( file_exists($dir."/wp-config.php") ) {
+			return $dir;
+		}
+	} while( $dir = realpath("$dir/..") );
+	return null;
+}
+
+define( 'BASE_PATH', findWordpressBasePath() . "/" );
+define( 'WP_USE_THEMES', false) ;
+
 global $wp, $wpdb, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header, $Polen_Plugin_Settings;
 
-include_once 'polen/plugins/polen_2/autoload.php';
-include_once 'polen/plugins/polen_2/vendor/autoload.php';
+include_once BASE_PATH . 'polen/plugins/polen_2/autoload.php';
+include_once BASE_PATH . 'polen/plugins/polen_2/vendor/autoload.php';
