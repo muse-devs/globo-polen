b83c627f794201f02d0215f9cc02813222fb19f7
Rodolfo Neto <rodolfoneto@gmail.com>
2021-04-23 14:16:28 -0300

    feat edit: ocultado os campos de "Video Enviado Por" quando a compra foi "Para Mim"

--- wordpress/polen/themes/polen/assets/js/cart.js
+++ wordpress/polen/themes/polen/assets/js/cart.js
@@ -3,11 +3,9 @@
         if( $(this).val() == 'to_myself' ) {
             $('.video-to-info').hide();
             $('input[name=offered_by]').prop( 'required', false );
-//            $('input[name=name_to_video]').prop( 'required', false );
         } else {
             $('.video-to-info').show();
             $('input[name=offered_by]').prop( 'required', true );
-//            $('input[name=name_to_video]').prop( 'required', true );
         }
     });
 })(jQuery);
