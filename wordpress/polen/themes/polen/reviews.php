d4c5fcc51996d10b9060db1f8cb46ca311fcf748
Rodolfo Neto <rodolfoneto>
2021-05-07 15:05:47 -0300

    feat add: review.php com dados de produto talent

--- wordpress/polen/themes/polen/reviews.php
+++ wordpress/polen/themes/polen/reviews.php
@@ -2,6 +2,10 @@
 
 get_header();
 
+$product = wc_get_product( $product_id );
+$product_post = get_post( $product->get_id() );
+$talent = get_user_by( 'id', $product_post->post_author );
+
 ?>
     <main id="primary" class="site-main">
         Oieeee Reviewssss Aquiii!!!!
