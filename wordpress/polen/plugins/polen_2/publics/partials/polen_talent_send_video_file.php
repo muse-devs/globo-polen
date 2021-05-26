58b820fafc9c7d3983a0d8ed10011338e359ba23
Rodolfo Neto <rodolfoneto>
2021-05-25 22:58:51 -0300

    REMOVE TESTE UPLOAD

--- wordpress/polen/plugins/polen_2/publics/partials/polen_talent_send_video_file.php
+++ wordpress/polen/plugins/polen_2/publics/partials/polen_talent_send_video_file.php
@@ -2,7 +2,7 @@
     <form id="form-video-upload" method="post" enctype="multipart/form-data">
         <div class="form-group">
             <label for="file-data">Example file input</label>
-            <input type="file" class="form-control-file" id="file-video" name="file_data" accept="video/*"> 
+            <input type="file" class="form-control-file" id="file-video" name="file_data" accept="video/*" capture="user">
         </div>
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
