3e619551ddfe191fb6f004b506d638097cd701ca
raorodrigues <raorodrigues@gmail.com>
2021-06-02 11:30:09 -0300

    layout busca de pedidos

--- wordpress/polen/plugins/polen_2/publics/partials/polen_order_tracking_anonimous_credentials.php
+++ wordpress/polen/plugins/polen_2/publics/partials/polen_order_tracking_anonimous_credentials.php
@@ -1,21 +1,28 @@
 <div id="primary" class="content-area cart-other">
     <main id="main" class="site-main" role="main">
+        <div class="row">
+            <div class="col-12 col-md-6 m-md-auto mb-3">
+                <h1>Número do pedido</h1>
+            </div>
+        </div>
         <form action="%1$s" method="post" class="form_search_order">
             %2$s
-            <div>
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <input id="fan_email" class="form-control form-control-lg" name="fan_email" value="" placeholder="E-mail" required="required" />
+            <div class="row">
+                <div class="col-12 col-md-6 m-md-auto">
+                    <div class="row">
+                        <div class="col-12">
+                            <input id="order_number" class="form-control form-control-lg" name="order_number" value="" placeholder="Número do pedido" required="required" />
+                        </div>
                     </div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <input id="order_number" class="form-control" name="order_number" value="" placeholder="Número do pedido" required="required" />
+                    <div class="row">
+                        <div class="col-12">
+                            <input id="fan_email" class="form-control form-control-lg" name="fan_email" value="" placeholder="E-mail" required="required" />
+                        </div>
                     </div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <button type="submit" class="btn btn-primary btn-lg btn-block py-4 btn-search-order" name="" value="">Buscar</button>
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block btn-search-order" name="" value="">Buscar</button>
+                        </div>
                     </div>
                 </div>
             </div>
