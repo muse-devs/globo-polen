37bbc2a1b1bff4797992df5751bd8539f2c15954
raorodrigues <raorodrigues@gmail.com>
2021-06-12 13:05:41 -0300

    nome do blog no header

--- wordpress/polen/themes/polen/header.php
+++ wordpress/polen/themes/polen/header.php
@@ -21,7 +21,7 @@ use Polen\Includes\Polen_Talent;
 	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="apple-mobile-web-app-title" content="Muse">
+	<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?>">
 	<meta name="apple-touch-fullscreen" content="yes">
 	<!-- <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> -->
 	<meta name="theme-color" content="#000000">
