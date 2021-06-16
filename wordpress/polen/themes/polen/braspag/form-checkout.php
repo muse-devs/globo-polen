29af6f1e0be5422d4338abfed58a9b803896b845
Andréa Tavares <andrea@cubo9.com.br>
2021-06-16 15:31:18 -0300

    start do cupom no checkout

--- wordpress/polen/themes/polen/braspag/form-checkout.php
+++ wordpress/polen/themes/polen/braspag/form-checkout.php
@@ -11,8 +11,9 @@ if( is_user_logged_in() ) {
     $braspag_card_saved_data = false;
 }
 ?>
-<div class="form-group" id="div_braspag_payment">
+<?php woocommerce_checkout_coupon_form(); ?>
 
+<div class="form-group" id="div_braspag_payment">
     <?php if( (int) $WC_Cubo9_BraspagReduxSettings['enable_installments'] === (int) 1 ) : ?>
 		<!-- Formas de Pagamento Parcelado -->
 		<div class="row">
