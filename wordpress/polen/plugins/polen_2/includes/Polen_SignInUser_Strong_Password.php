b50e1724e707f25079c2e1312133b0b9803624f1
Rodolfo Neto <rodolfoneto>
2021-06-24 18:04:18 -0300

    feat edit: diminuindo as necessidades da senha no cadastro do fã

--- wordpress/polen/plugins/polen_2/includes/Polen_SignInUser_Strong_Password.php
+++ wordpress/polen/plugins/polen_2/includes/Polen_SignInUser_Strong_Password.php
@@ -38,13 +38,14 @@ class Polen_SignInUser_Strong_Password
 
         $password_lenght = 6;
         // Validate password strength
-        $uppercase = preg_match('@[A-Z]@', $password);
-        $lowercase = preg_match('@[a-z]@', $password);
+        $uppercase = true;//preg_match('@[A-Z]@', $password);
+        $lowercase = true;//preg_match('@[a-z]@', $password);
         $number    = preg_match('@[0-9]@', $password);
-        $specialChars = preg_match('@[^\w]@', $password);
+        $specialChars = true;//preg_match('@[^\w]@', $password);
 
         if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < $password_lenght) {
-            $errors->add( 'registration-error', "A senha deveria ter ao menos {$password_lenght} caracteres, 1 letra maiuscula, 1 numero e 1 caracter especial", 'woocommerce' );
+            // $errors->add( 'registration-error', "A senha deveria ter ao menos {$password_lenght} caracteres, 1 letra maiuscula, 1 numero e 1 caracter especial", 'woocommerce' );//Antiga mensagem quando ainda precisava de uppercase, lowercase e specialChars
+            $errors->add( 'registration-error', "A senha deveria ter ao menos {$password_lenght} caracteres e 1 numero", 'woocommerce' );
         }
 
         return $errors;
