0e35abe3e1ee28589a2a52921463311498a97741
Rodolfo Neto <rodolfoneto>
2021-07-02 09:36:31 -0300

    feat add: arquivo de email e envio de teste.

---
+++ wordpress/polen/themes/polen/tributes/emails/invite_send_video.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Convite para Tributo</title>
+</head>
+<body>
+    <p>Oi %s,</p>
+    <p>%s o convidou a participar da criação de uma vídeo montagem para %s.</p>
+    <p><strong>Seu prazo para envio é %s.</strong></p>
+
+    <p>Mensagem de boas-vindas e instruções de %s:</p>
+    <p>%s</p>
+
+    <p>Perguntas a serem respondidas em seu vídeo:</p>
+    <p>%d</p>
+
+    <p>Para saber mais e enviar seu vídeo, clique no botão abaixo.</p>
+    <p>Leva apenas um minuto e você pode fazer isso de qualquer dispositivo.</p>
+    <a href="%s">Enviar Tributo</a>
+    <img src="%s" />
+</body>
+</html>
