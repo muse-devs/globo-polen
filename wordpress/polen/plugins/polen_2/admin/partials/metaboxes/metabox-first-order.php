0d01dd7e9ed4062956a03ab8b7a2ee8659b30d64
Rodolfo Neto <rodolfoneto>
2021-07-06 23:10:13 -0300

    ----

--- wordpress/polen/plugins/polen_2/admin/partials/metaboxes/metabox-first-order.php
+++ wordpress/polen/plugins/polen_2/admin/partials/metaboxes/metabox-first-order.php
@@ -6,14 +6,15 @@ defined( 'ABSPATH' ) || die;
         <a href="#" id="polen_create_first_order" onclick="polen_create_first_order()">Criar uma Order</a>
     </div>
     <div class="clear"></div>
+    <form id="form-create-first-order">
+        <input type="hidden" name="product_id" />
+    </form>
 </div>
 <script>
 document.getElementById('polen_create_first_order').addEventListener("click", function(evt){
     evt.preventDefault();
     let url = '<?= admin_url('admin-ajax.php'); ?>?action=create_first_order';
-    jQuery.post(url,{
-
-    },
+    jQuery.post(url,jQuery(formCreateName).serialize(),
     function(data){
 
     });
