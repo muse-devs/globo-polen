fd4e80c3e59d8d88d3bae7cd380a43c135ac9cc2
raorodrigues <raorodrigues@gmail.com>
2021-07-07 18:27:15 -0300

    compress

--- wordpress/polen/themes/polen/assets/js/upload-video-tributes.js
+++ wordpress/polen/themes/polen/assets/js/upload-video-tributes.js
@@ -44,7 +44,7 @@ window.onload = () => {
 		upload_video.file_size = file_input.files[0].size.toString();
 		jQuery
 			.post(
-				woocommerce_params.ajax_url + "?action=upload-video-tribute",
+				woocommerce_params.ajax_url + "?action=make_video_slot_vimeo",
 				upload_video,
 				(data, textStatus, jqXHR) => {
 					if (jqXHR.status == 200) {
