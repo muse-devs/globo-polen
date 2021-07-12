bc2ea46cec69cfb8ae0419533095751c279192a8
raorodrigues <raorodrigues@gmail.com>
2021-07-12 19:39:12 -0300

    layout nos 3

--- wordpress/polen/themes/polen/tributes/emails/complete_tribute.php
+++ wordpress/polen/themes/polen/tributes/emails/complete_tribute.php
@@ -1,18 +1,44 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Convite para Tributo</title>
-</head>
-<body>
-<h1>Seu Colab está pronto</h1>
-<p>IMAGO DO EMAIL</p>
-<p>Oi %s,</p>
-<p>Sua homenagem está pronta, quem participou do tributo foi: </p>
-<p>%s</p>
-
-<a href="%s">Ver vídeo de presente</a>
-</body>
-</html>
+<tbody>
+	<table style="margin: auto; width: 98%%; max-width: 720px">
+		<tr>
+			<td style="height: 120px">
+				<h1 style="text-align: center">
+					Seu Colab está pronto
+				</h1>
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 200px; text-align: center">
+				<img width="185" height="185" src="%s" alt="Ícone de sucesso" />
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 40px"></td>
+		</tr>
+		<tr>
+			<td id="content">
+				<p>Oi %s,</p>
+				<p>Sua homenagem está pronta, quem participou do tributo foi: </p>
+				<p>%s</p>
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 60px"></td>
+		</tr>
+		<tr>
+			<td style="text-align: center">
+				<a href="%s" style="
+									padding: 15px 100px;
+									font-site: 14px;
+									text-decoration: none;
+									color: white;
+									background-color: #fd6c36;
+									border-radius: 8px;
+								" target="_blank">Ver vídeo de presente</a>
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 80px"></td>
+		</tr>
+	</table>
+</tbody>
--- wordpress/polen/themes/polen/tributes/emails/invites_sended.php
+++ wordpress/polen/themes/polen/tributes/emails/invites_sended.php
@@ -1,23 +1,49 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Convite para Tributo</title>
-</head>
-<body>
-<h1>Seus convites foram enviados</h1>
-<p>IMAGO DO EMAIL</p>
-<p>Oi %s,</p>
-<p>seu convite foi enviado para: </p>
-<p>%s</p>
+<tbody>
+	<table style="margin: auto; width: 98%%; max-width: 720px">
+		<tr>
+			<td style="height: 120px">
+				<h1 style="text-align: center">
+					Seus convites foram enviados
+				</h1>
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 200px; text-align: center">
+				<img width="185" height="185" src="%s" alt="Ícone de carrossel de vídeos" />
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 40px"></td>
+		</tr>
+		<tr>
+			<td id="content">
+				<p>Oi %s,</p>
+				<p>seu convite foi enviado para: </p>
+				<p>%s</p>
 
-<p>O prazo para vídeo estar pronto é %s.</p>
+				<p>O prazo para vídeo estar pronto é %s.</p>
 
-<p>Mensagem de boas-vindas e instruções de %d:</p>
-<p>%d</p>
-
-<a href="%s">Revisar convites</a>
-</body>
-</html>
+				<p>Mensagem de boas-vindas e instruções de %d:</p>
+				<p>%d</p>
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 60px"></td>
+		</tr>
+		<tr>
+			<td style="text-align: center">
+				<a href="%s" style="
+									padding: 15px 100px;
+									font-site: 14px;
+									text-decoration: none;
+									color: white;
+									background-color: #fd6c36;
+									border-radius: 8px;
+								" target="_blank">Revisar Convites</a>
+			</td>
+		</tr>
+		<tr>
+			<td style="height: 80px"></td>
+		</tr>
+	</table>
+</tbody>
