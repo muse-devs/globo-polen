7d39f6ad51b0df1431c3fb8e29b4011f84d4faed
raorodrigues <raorodrigues@gmail.com>
2021-07-26 17:34:17 -0300

    banner criesp home

--- wordpress/polen/themes/polen/inc/home.php
+++ wordpress/polen/themes/polen/inc/home.php
@@ -14,6 +14,8 @@ get_header();
 
 	<?php polen_banner_scrollable(polen_get_new_talents(6), "Destaque", polen_get_all_new_talents_url()); ?>
 
+	<?php criesp_get_home_banner("#criesp"); ?>
+
 	<?php polen_front_get_tutorial(); ?>
 
 	<?php polen_front_get_news(polen_get_talents(12), "Todos os talentos", polen_get_all_talents_url());
