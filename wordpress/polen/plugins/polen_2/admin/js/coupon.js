207579cb4a6d3316772526201ced43894442cd6d
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2021-08-10 14:20:31 -0300

    funcionando

--- wordpress/polen/plugins/polen_2/admin/js/coupon.js
+++ wordpress/polen/plugins/polen_2/admin/js/coupon.js
@@ -1,24 +1,99 @@
 "use strict";
 
+const MESSAGES = {
+  INITIAL: "Criar",
+  WAIT: "Enviando...",
+  DONE: "Enviado!",
+};
+
+const DISCOUNT_TYPES = [
+  {
+    NAME: "Fixo no carrinho",
+    TYPE: "fixed_cart",
+    SYMBOL: "R$",
+  },
+  {
+    NAME: "Percentual no carrinho",
+    TYPE: "percent",
+    SYMBOL: "%",
+  },
+  {
+    NAME: "Fixo no produto",
+    TYPE: "fixed_product",
+    SYMBOL: "R$",
+  },
+  {
+    NAME: "Percentual no produto",
+    TYPE: "percent_product",
+    SYMBOL: "%",
+  },
+];
+
+function getSymbol(type) {
+  const item = DISCOUNT_TYPES.filter((ITEM) => (ITEM.TYPE === type));
+  return item[0].SYMBOL;
+}
+
+const maskDate = (value) => {
+  let v = value.replace(/\D/g, "").slice(0, 10);
+  if (v.length >= 5) {
+    return `${v.slice(0, 2)}/${v.slice(2, 4)}/${v.slice(4)}`;
+  } else if (v.length >= 3) {
+    return `${v.slice(0, 2)}/${v.slice(2)}`;
+  }
+  return v;
+};
+
+const createCoupon = (data) => {
+  const send_data = {
+    prefix_name: data.prefix_name,
+    amount: data.amount,
+    discount_type: data.discount_type,
+    description: data.description,
+    expiry_date: data.expiry_date,
+    usage_limit: data.usage_limit,
+  };
+  return new Promise((resolve, reject) => {
+    jQuery
+      .post(
+        "/wp-admin/admin-ajax.php?action=polen_create_cupom",
+        send_data,
+        function (data) {
+          resolve(data);
+        }
+      )
+      .fail(function (xhr, status, error) {
+        reject(error);
+      });
+  });
+};
+
 const batch_coupon = new Vue({
   el: "#batch-coupon",
   data: {
     prefix_name: "",
     amount: 0,
-    discount_type: "percent",
-    distount_type_list: [
-      "fixed_cart",
-      "percent",
-      "fixed_product",
-      "percent_product",
-    ],
+    discount_type: "",
+    distount_type_list: DISCOUNT_TYPES,
     description: "",
     expiry_date: "",
-    usage_limit: "1"
+    usage_limit: "1",
+    symbol: "",
+    message: MESSAGES.INITIAL,
   },
   methods: {
-      createCoupon: function() {
-          
-      }
-  }
+    validateDate: function () {
+      this.expiry_date = maskDate(this.expiry_date);
+    },
+    handleChangeSelect: function () {
+      this.symbol = getSymbol(this.discount_type);
+    },
+    createCoupon: function () {
+      this.message = MESSAGES.WAIT;
+      createCoupon(this).then((ret) => {
+        console.log(ret);
+        this.message = MESSAGES.INITIAL;
+      });
+    },
+  },
 });
--- wordpress/polen/plugins/polen_2/includes/Polen_Cupom_Create_Admin_Menu.php
+++ wordpress/polen/plugins/polen_2/includes/Polen_Cupom_Create_Admin_Menu.php
@@ -23,37 +23,47 @@ class Polen_Cupom_Create_Admin_Menu
         wp_enqueue_script('batch-coupon', Polen_Admin::get_js_url('coupon.js'), array('jquery', 'vuejs'), DEVELOPER ?  time() : "1.0.0", false);
 ?>
         <h1>Cupons em Lote</h1>
-        <div id="batch-coupon" class="container mt-4">
-            <form>
+        <div id="batch-coupon" class="container batch-coupon mt-4">
+            <form v-on:submit.prevent="createCoupon">
                 <input type="hidden" name="action" value="polen_create_cupom" />
                 <div class="row mb-2">
                     <div class="col">
-                        <input type="text" v-model="prefix_name" placeholder="prefixo" required />
-                        <input type="number" v-model="amount" placeholder="valor" required />
+                        <label for="prefix_name" class="bc-label">Prefixo</label>
+                        <input type="text" id="prefix_name" v-model="prefix_name" placeholder="prefixo" required />
+                    </div>
+                    <div class="col">
+                        <label for="amount" class="bc-label">Valor do Cupom<span v-if="symbol" class="ml-1">{{symbol}}</span></label>
+                        <input type="number" id="amount" v-model="amount" placeholder="valor" required />
                     </div>
                 </div>
                 <div class="row mb-2">
                     <div class="col">
-                        <select v-model="discount_type" required>
-                            <option v-for="type in distount_type_list">{{type}}</option>
+                        <label for="discount_type" class="bc-label">Tipo de Desconto</label>
+                        <select v-model="discount_type" v-on:change="handleChangeSelect" id="discount_type" required>
+                            <option disabled value="">Escolha um item</option>
+                            <option v-for="option in distount_type_list" v-bind:value="option.TYPE">{{option.NAME}}</option>
                         </select>
                     </div>
                 </div>
                 <div class="row mb-2">
                     <div class="col">
-                        <textarea cols="30" rows="10" v-model="description" placeholder="descrição" required></textarea>
+                        <label for="description" class="bc-label">Descrição</label>
+                        <textarea cols="30" rows="10" id="description" v-model="description" placeholder="descrição" required></textarea>
                     </div>
                 </div>
                 <div class="row mb-2">
                     <div class="col">
-                        <input type="text" v-model="expiry_date" placeholder="01/11/2021" required />
-                        <input type="text" v-model="usage_limit" placeholder="limite" required />
-
+                        <label for="expiry_date" class="bc-label">Data de expiração</label>
+                        <input type="text" id="expiry_date" v-on:keyup="validateDate" v-model="expiry_date" placeholder="01/11/2021" maxlength="10" required />
+                    </div>
+                    <div class="col">
+                        <label for="usage_limit" class="bc-label">Limite de uso</label>
+                        <input type="text" id="usage_limit" v-model="usage_limit" placeholder="limite" required />
                     </div>
                 </div>
                 <div class="row">
                     <div class="col">
-                        <input type="submit" value="Criar" />
+                        <button type="submit" class="btn-send">{{message}}</button>
                     </div>
                 </div>
             </form>
