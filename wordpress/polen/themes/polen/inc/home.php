dc1773aa7afe2222ed705f2e0245d53d9fee60f8
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2021-08-17 16:21:09 -0300

    bkp

--- wordpress/polen/themes/polen/inc/home.php
+++ wordpress/polen/themes/polen/inc/home.php
@@ -12,6 +12,8 @@ get_header();
 	<?php polen_front_get_banner_with_carousel();
 	?>
 
+	<?php va_get_book_infos(); ?>
+
 	<?php polen_banner_scrollable(social_get_products_by_category_slug(social_get_category_base()), "Criança Esperança", social_get_criesp_url(), "Aqui seu Vídeo Polen vira uma doação", true); ?>
 
 	<?php criesp_get_home_banner(social_get_criesp_url()); ?>
