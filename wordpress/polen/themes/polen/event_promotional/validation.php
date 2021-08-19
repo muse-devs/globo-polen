84a90b85e5eef1d936934a49fcc8c2edc326d230
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2021-08-19 11:04:02 -0300

    layout da página de cupon e do form de checkout

--- wordpress/polen/themes/polen/event_promotional/validation.php
+++ wordpress/polen/themes/polen/event_promotional/validation.php
@@ -1,1 +1,32 @@
-VALIDACAO!!!
+<?php
+
+/**
+ * Template name: Página Inicial Vídeo Autógrafo
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+	<div class="row">
+		<div class="col-12 col-md-8 m-md-auto">
+			<?php
+			va_get_banner_book(true);
+			va_magalu_box_cart();
+			?>
+			<div class="row mb-3">
+				<div class="col-12">
+					<h1 class="title mb-3">Inserir código</h1>
+					<form id="va-check-code">
+						<input type="hidden" name="action" value="" />
+						<input type="text" class="form-control form-control-lg mb-2" placeholder="Inserir código fornecido pela Magalu" required />
+						<input type="submit" class="btn btn-primary btn-lg btn-block" value="Checar" />
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</main><!-- #main -->
+
+<?php
+get_footer();
--- wordpress/polen/themes/polen/functions.php
+++ wordpress/polen/themes/polen/functions.php
@@ -234,6 +234,7 @@ function polen_scripts() {
 
 	wp_enqueue_style('polen-custom-styles', TEMPLATE_URI . '/assets/css/style.css', array(), filemtime(TEMPLATE_DIR . '/assets/css/style.css'));
 
+	//TODO - Colocar função para identificar video-tributo
 	if((is_singular() && is_product()) || is_page('video-autografo')) {
 		// wp_enqueue_script( 'slick-slider', TEMPLATE_URI . '/assets/slick/slick.min.js', array("jquery"), '', true );
 		wp_enqueue_script( 'vimeo');
