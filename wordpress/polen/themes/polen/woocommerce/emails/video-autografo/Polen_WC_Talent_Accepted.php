ec0d397ff0b00714b8609a2609384d615429e575
Ian Silva <iansilva6@yahoo.com.br>
2021-08-20 11:41:41 -0300

    fix: corrigindo e-mail

--- wordpress/polen/themes/polen/woocommerce/emails/video-autografo/Polen_WC_Talent_Accepted.php
+++ wordpress/polen/themes/polen/woocommerce/emails/video-autografo/Polen_WC_Talent_Accepted.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                 <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
                     <tr>
                     <td align="center" valign="top">
-                        <div id="template_header_image" style="display: flex; justify-content: center; align-items: center;">
+                        <div style="display: flex; justify-content: center; align-items: center;">
                         <img src="<?php echo TEMPLATE_URI.'/assets/img/video-autografo/polen-email.png'?>" style="height: 50px; margin: 1em;"></img>
                             <img src="<?php echo TEMPLATE_URI.'/assets/img/video-autografo/magalu-email.png'?>" style="height: 30px; margin: 1em;"></img>
                             <img src="<?php echo TEMPLATE_URI.'/assets/img/video-autografo/cia-email.png'?>" style="height: 64px; margin: 1em;"></img>
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                             <tr>
                                 <td align="center" valign="top">
                                     <!-- Header -->
-                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header" style='color: #ffffff; border-bottom: 0; font-weight: bold; line-height: 100%; vertical-align: middle; font-family: Poppins, "Helvetica Neue", Helvetica, Arial, sans-serif;'>
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style='color: #ffffff; border-bottom: 0; font-weight: bold; line-height: 100%; vertical-align: middle; font-family: Poppins, "Helvetica Neue", Helvetica, Arial, sans-serif;'>
                                         <tr>
                                             <td style="display: flex; justify-content: center;border-bottom: 1px solid rgba(255, 255, 255, 0.2);">
                                                 <img src="<?php echo TEMPLATE_URI.'/assets/img/video-autografo/book_cover.png'?>" style="height: 250px;"></img>
@@ -41,7 +41,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                                     <!-- Body -->
                                     <table border="0" cellpadding="0" cellspacing="0" width="600" id="template_body">
                                         <tr>
-                                            <td valign="top" id="body_content" style="background-color: #0183CB;">
+                                            <td valign="top" style="background-color: #0183CB;">
                                                 <!-- Content -->
                                                 <table border="0" cellpadding="20" cellspacing="0" width="100%">
                                                     <tr>
