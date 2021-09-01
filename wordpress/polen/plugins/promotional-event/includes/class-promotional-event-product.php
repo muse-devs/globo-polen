4405384fa486d75c0ff5b53debdfdeae208cb15f
Rodolfo Neto <rodolfoneto>
2021-08-31 22:57:39 -0300

    feat edit: Corrigindo o SEO do Livro-Autografo pra vários produtos

--- wordpress/polen/plugins/promotional-event/includes/class-promotional-event-product.php
+++ wordpress/polen/plugins/promotional-event/includes/class-promotional-event-product.php
@@ -8,4 +8,14 @@ class Promotional_Event_Product
     {
         $this->product = $product;
     }
+
+    public function get_url_image_book()
+    {
+        $attachment_id = $this->product->get_image_id();
+        $src = wp_get_attachment_image_src( $attachment_id, 'polen-thumb-lg' );
+        if( empty( $src ) ) {
+            return false;
+        }
+        return $src[ 0 ];
+    }
 }
