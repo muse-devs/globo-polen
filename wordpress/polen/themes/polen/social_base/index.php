48fbfc511bd7399582a0e77d9b5c9d196be325b5
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2021-09-02 18:45:38 -0300

    layout

--- wordpress/polen/themes/polen/social_base/index.php
+++ wordpress/polen/themes/polen/social_base/index.php
@@ -1,5 +1,7 @@
 <?php
 
+defined('ABSPATH') || die;
+
 use Polen\Social_Base\Social_Base_Product;
 use Polen\Social_Base\Social_Base_Rewrite;
 
@@ -7,6 +9,25 @@ $social_slug = $GLOBALS[ Social_Base_Rewrite::QUERY_VARS_SOCIAL_SLUG ];
 
 $products = Social_Base_Product::get_all_products_by_slug_campaing( $social_slug );
 
-var_dump( $products );
+get_header();
+?>
 
-echo $social_slug;
+<main id="primary" class="site-main">
+
+	<?php sa_get_modal(); ?>
+
+	<?php polen_front_get_banner_with_carousel(true);
+	?>
+
+	<?php polen_front_get_news(social_get_products_by_category_slug(social_get_category_base()), "Os artistas que apoiam essa causa", null, true);
+	?>
+
+	<?php polen_front_get_tutorial(); ?>
+
+	<?php //polen_front_get_suggestion_box();
+	?>
+
+</main><!-- #main -->
+
+<?php
+get_footer();
