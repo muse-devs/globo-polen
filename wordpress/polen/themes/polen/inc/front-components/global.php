f4687b6c200523d19badbefc91c8233cc75b187a
Ian Silva <iansilva6@yahoo.com.br>
2021-09-02 17:38:36 -0300

    feat: adicionando banner do SA no meio do site

--- wordpress/polen/themes/polen/inc/front-components/global.php
+++ wordpress/polen/themes/polen/inc/front-components/global.php
@@ -37,23 +37,23 @@ function polen_front_get_banner_with_carousel($social = false)
 						</a>
 					</div>
 				</div>
-				<div class="item">
+				<!-- <div class="item">
 					<div class="carrousel">
-						<?php foreach ($carrousel2 as $item) : ?>
+						<?php //foreach ($carrousel2 as $item) : ?>
 							<figure class="image">
-								<img loading="lazy" src="<?php echo $item['mobile']; ?>" alt="Banner da home" class="mobile" />
-								<img loading="lazy" src="<?php echo $item['desktop']; ?>" alt="Banner da home" class="desktop" />
+								<img loading="lazy" src="<?php //echo $item['mobile']; ?>" alt="Banner da home" class="mobile" />
+								<img loading="lazy" src="<?php //echo $item['desktop']; ?>" alt="Banner da home" class="desktop" />
 							</figure>
-						<?php endforeach; ?>
+						<?php //endforeach; ?>
 					</div>
 					<div class="content">
 						<h2 class="title mb-5">Setembro é o mês da prevenção ao suicídio.<br>Agir salva vidas!</h2>
-						<a href="<?php echo polen_get_all_talents_url(); ?>" class="banner-button-link button-yellow">
+						<a href="<?php //echo polen_get_all_talents_url(); ?>" class="banner-button-link button-yellow">
 							<span class="mr-3">Veja os depoimentos</span>
-							<?php Icon_Class::polen_icon_chevron_right(); ?>
+							<?php //Icon_Class::polen_icon_chevron_right(); ?>
 						</a>
 					</div>
-				</div>
+				</div> -->
 			<?php endif; ?>
 		</div>
 	</section>
