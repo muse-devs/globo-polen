d2bf1a3fb84cc503937f3888fa8f4065b45ba5c7
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2021-09-06 14:54:10 -0300

    base front

--- wordpress/masterclass/wp-content/themes/2021_beaba_do_vinho/setup/assets.php
+++ wordpress/masterclass/wp-content/themes/2021_beaba_do_vinho/setup/assets.php
@@ -1,16 +1,36 @@
 <?php
+define('TEMPLATE_URI', get_template_directory_uri());
+define('TEMPLATE_DIR', get_template_directory());
+define('DEVELOPER', defined('ENV_DEV') && ENV_DEV);
+
+function get_assets_folder() {
+	$min = "min/";
+	if (DEVELOPER) {
+		$min = "";
+	}
+	return $min;
+}
 
 /**
  * Registra os arquivos de CSS e JS do tema
  */
 function _theme_assets()
 {
-    $uri = get_template_directory_uri();
-    wp_enqueue_style('_theme-style', $uri . '/assets/css/default-woocommerce.css');
-    wp_enqueue_script('_theme-js-main', $uri . '/assets/js/develop.js', array(), null, true);
+    $min = get_assets_folder();
+    wp_enqueue_style('_theme-style', TEMPLATE_URI . '/assets/css/default-woocommerce.css');
+    wp_enqueue_script('_theme-js-main', TEMPLATE_URI. '/assets/js/develop.js', array(), null, true);
 
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
+
+    // Arquivos front-end
+    wp_enqueue_style('_theme-style-front', TEMPLATE_URI . '/assets/css/style.css', array(), filemtime(TEMPLATE_DIR . '/assets/css/style.css'));
+    wp_enqueue_script('_theme-js-front', TEMPLATE_URI . '/assets/js/' . $min . 'global.js', array("jquery"), filemtime(TEMPLATE_DIR . '/assets/js/global.js'), false);
 }
 add_action('wp_enqueue_scripts', '_theme_assets');
+
+
+// Componentes Front
+require_once TEMPLATE_DIR . '/classes/Icon_Class.php';
+require TEMPLATE_DIR . '/inc/components.php';
