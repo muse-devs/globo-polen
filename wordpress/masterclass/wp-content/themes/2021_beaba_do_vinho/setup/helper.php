c132c5930e1e687ea5be8e5ab285a55a67abce74
Glaydson Rodrigues <glaydson.queiroz@polen.me>
2021-09-09 11:54:05 -0300

    Endpoint para webhook

--- wordpress/masterclass/wp-content/themes/2021_beaba_do_vinho/setup/helper.php
+++ wordpress/masterclass/wp-content/themes/2021_beaba_do_vinho/setup/helper.php
@@ -187,3 +187,45 @@ add_filter( 'site_transient_update_plugins', 'filter_plugin_updates' );
 add_filter('woocommerce_is_sold_individually', function () {
     return true;
 }, 9999, 2);
+
+
+/**
+ * Função para request webwook
+ *
+ * @params email // required
+ * @route admin-ajax.php?action=send_form_request
+ */
+function send_form_request()
+{
+    try{
+        $email = sanitize_text_field($_POST['email']);
+
+        if( !filter_input( INPUT_POST, 'email', FILTER_VALIDATE_EMAIL ) ) {
+            wp_send_json_error( 'Email incorreto', 422 );
+        }
+
+        $url = 'https://hooks.zapier.com/hooks/catch/10583855/b483m4i/';
+        $response = wp_remote_post($url, array(
+                'method' => 'POST',
+                'timeout' => 45,
+                'headers' => array(),
+                'body' => array(
+                    'email' => $email,
+                ),
+            )
+        );
+
+        if (is_wp_error($response)) {
+            wp_send_json_error( 'Sistema indisponível. Por favor entre em contato com o suporte', 503 );
+            wp_die();
+        }
+
+        wp_send_json_success( 'ok', 200 );
+
+    } catch (\Exception $e) {
+        wp_send_json_error($e->getMessage(), 422);
+        wp_die();
+    }
+}
+add_action('wp_ajax_send_form_request', 'send_form_request');
+add_action('wp_ajax_nopriv_send_form_request', 'send_form_request');
