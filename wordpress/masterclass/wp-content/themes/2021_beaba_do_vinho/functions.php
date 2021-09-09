ba10feded7551194cf56f3f02ee8f6d51f614702
Rodolfo Neto <rodolfoneto>
2021-09-09 20:24:00 -0300

    feat edit: corrigiindo SMTP (de novo)

--- wordpress/masterclass/wp-content/themes/2021_beaba_do_vinho/functions.php
+++ wordpress/masterclass/wp-content/themes/2021_beaba_do_vinho/functions.php
@@ -25,34 +25,6 @@ function _theme_add_woocommerce_support()
 
 add_action('after_setup_theme', '_theme_add_woocommerce_support');
 
-<?php
-$files = array(
-    'setup/assets.php',
-    'setup/core.php',
-    'setup/helper.php',
-    'setup/post-types.php',
-    'setup/functions_front/render.php',
-);
-
-foreach ($files as $file) {
-    if (!$filePath = locate_template($file)) {
-        error_log(sprintf(__('Erro ao carregar arquivo : %s'), $file), E_USER_ERROR);
-    }
-    include_once $filePath;
-}
-
-/**
- * Adicionar suporte para que o tema possa modificar os arquivos
- * de configuração padrão do woocommerce
- */
-function _theme_add_woocommerce_support()
-{
-    add_theme_support('woocommerce');
-}
-
-add_action('after_setup_theme', '_theme_add_woocommerce_support');
-
-
 add_action( 'phpmailer_init', 'masterclass_load_settings');
 
 function masterclass_load_settings() {
@@ -67,4 +39,3 @@ function masterclass_load_settings() {
     $phpmailer->From       = 'no-reply@polen.me';
     $phpmailer->FromName   = 'Polen';
 }
-
