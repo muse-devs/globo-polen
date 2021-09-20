d88dfff7a0dc894d60fe8e890217771aee73cd5a
Rodolfo Neto <rodolfoneto>
2021-09-20 16:54:13 -0300

    feat edit: melhorando order deadline

--- wordpress/polen/plugins/polen_2/cron/cron_create_deadline.php
+++ wordpress/polen/plugins/polen_2/cron/cron_create_deadline.php
@@ -1,81 +1,92 @@
 <?php
-use Polen\Includes\Debug;
 
 include_once dirname( __FILE__ ) . '/init.php';
 
 $args = [
-    'return' => 'ids',
+//     'fields' => 'ids',
     'paginate' => true,
-    'per_page' => '10',
+    'limit' => '100',
     'order_by' => 'id',
-    'order' => 'ASC',
-    'page' => 1,
-    // 'post_type' => wc_get_order_types(),
+    'order' => 'DESC',
+//     'page' => 1,
+//     'post_type' => wc_get_order_types(),
     'post_status' => array_keys( wc_get_order_statuses() ),
-    // 'post_status' => ['wc-payment-approved','wc-talent-accepted'],
-    'deadline' => '2021-08-13',
-    // 'meta_query' => [
-    //     [ 'key' => '_deadline', 'value' => '1629470548', 'compare' => 'IN' ],
-    // ],
+//     // 'post_status' => ['wc-payment-approved','wc-talent-accepted'],
+//     // 'deadline' => '2021-08-13',
+//     'meta_query' => [
+//         [ 'key' => '_deadline', 'value' => '1629470548', 'compare' => 'IN' ],
+//     ],
 ];
 
 // $oq = new WP_Query( $args );
-$oq = new WC_Order_Query( $args );
+// $oq = new WC_Order_Query( $args );
+// $oq = new Polen_Order_Data_Store_CPT( $args );
 
 
 // var_dump( $oq->query_vars);//die;
-$orders = $oq->get_orders();
-// $orders = $oq->get_posts();
-global $wpdb;
-var_dump( $wpdb->last_query );
-// var_dump('FOUND_ROWS: '.$wpdb->get_var('SELECT FOUND_ROWS();'));
+// $orders = $oq->get_orders();
+$orders = wc_get_orders( $args );
 
-// $ds = new WC_Data_Store_WP();
-// var_dump( $ds-> );
+for( $i = 1; $i <= $orders->max_num_pages; $i++ ) :
+    // $orders = $oq->get_posts();
+    // global $wpdb;
+    // var_dump( $wpdb->last_query );
+    // var_dump('FOUND_ROWS: '.$wpdb->get_var('SELECT FOUND_ROWS();'));
 
-
-
-
-// Debug::def( $oq->get_query_vars() );die;
-// var_dump($oq->get_query_vars());
-// $ods = new WC_Order_Data_Store_CPT();
-// var_dump( $ods->parse_date_for_wp_query( '<YYYY-MM-DD','_deadline', ['value' => '2021-08-13'] ) );
-// var_dump( WC_Data_Store::load( 'order' )->get_current_class_name() );
-// var_dump( $oq->parse_date_for_wp_query() );
+    // $ds = new WC_Data_Store_WP();
+    // var_dump( $ds-> );
 
 
-// die;
-// var_dump( $oq->parse_date_for_wp_query() );die;
-var_dump( $orders );
-die;
-// var_dump($orders);
-foreach( $orders as $order ) {
-    if( social_order_is_social( $order ) ) {
-        $interval_time = new DateInterval( 'P15D' );
-        $type = ' S ';
-    } elseif ( event_promotional_order_is_event_promotional( $order ) ) {
-        $interval_time = new DateInterval( 'P30D' );
-        $type = ' E ';
-    } else {
-        $interval_time = new DateInterval( 'P7D' );
-        $type = ' B ';
+
+
+    // Debug::def( $oq->get_query_vars() );die;
+    // var_dump($oq->get_query_vars());
+    // $ods = new WC_Order_Data_Store_CPT();
+    // var_dump( $ods->parse_date_for_wp_query( '<YYYY-MM-DD','_deadline', ['value' => '2021-08-13'] ) );
+    // var_dump( WC_Data_Store::load( 'order' )->get_current_class_name() );
+    // var_dump( $oq->parse_date_for_wp_query() );
+
+
+    // die;
+    // var_dump( $oq->parse_date_for_wp_query() );die;
+    // var_dump( $orders );
+    // die;
+    var_dump($orders);
+    foreach( $orders->orders as $order ) {
+        if( social_order_is_social( $order ) ) {
+            $interval_time = new DateInterval( 'P15D' );
+            $type = ' S ';
+        } elseif ( event_promotional_order_is_event_promotional( $order ) ) {
+            $interval_time = new DateInterval( 'P30D' );
+            $type = ' E ';
+        } else {
+            $interval_time = new DateInterval( 'P7D' );
+            $type = ' B ';
+        }
+
+        $created_at = $order->get_date_created();
+        echo $order->get_id();
+        echo '  |  ';
+        echo $created_at->format('d/m/Y H:i:s');
+        echo ' | ';
+        echo $created_at->add( $interval_time )->format('d/m/Y H:i:s');
+        echo ' | ';
+        echo $created_at->getTimestamp();
+        echo $type;
+        echo "\n";
+        // $order->add_meta_data( '_polen_deadline', $created_at->getTimestamp(), true );
+        // $order->save();
     }
 
-    $created_at = $order->get_date_created();
-    echo $order->get_id();
-    echo '  |  ';
-    echo $created_at->format('d/m/Y H:i:s');
-    echo ' | ';
-    echo $created_at->add( $interval_time )->format('d/m/Y H:i:s');
-    echo ' | ';
-    echo $created_at->getTimestamp();
-    echo $type;
-    echo "\n";
-    // $order->add_meta_data( '_polen_deadline', $created_at->getTimestamp(), true );
-    // $order->save();
-}
-
+    $args = [
+            'paginate' => true,
+            'per_page' => '100',
+            'order_by' => 'id',
+            'order' => 'DESC',
+            'post_status' => array_keys( wc_get_order_statuses() ),
+    ];
+endfor;
 // var_dump( $wpdb->last_query );
 
 // var_dump( $orders );
-WC_Emails::instance();
+// WC_Emails::instance();
