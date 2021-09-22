cda9e618d886b2d00fbd203ed43c9b0247b6c797
Rodolfo Neto <rodolfoneto>
2021-09-22 11:23:38 -0300

    UPDATE REDUT

--- wordpress/polen/plugins/redux-framework/redux-framework.php
+++ wordpress/polen/plugins/redux-framework/redux-framework.php
@@ -4,22 +4,24 @@
  * for WordPress themes and plugins. Developed with WordPress coding
  * standards and PHP best practices in mind.
  *
- * Plugin Name:     Redux
- * Plugin URI:      http://wordpress.org/plugins/redux-framework
- * GitHub URI:      reduxframework/redux-framework
- * Description:     Build better sites in WordPress fast
- * Author:          Redux.io
- * Author URI:      http://redux.io
- * Version:         4.2.14
- * Text Domain:     redux-framework
- * License:         GPLv3 or later
- * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
- * Provides:        ReduxFramework
+ * Plugin Name:         Redux
+ * Plugin URI:          http://wordpress.org/plugins/redux-framework
+ * GitHub URI:          reduxframework/redux-framework
+ * Description:         Build better sites in WordPress fast!
+ * Version:             4.3.0
+ * Requires at least:   4.0
+ * Requires PHP:        7.1
+ * Author:              Extendify
+ * Author URI:          https://extendify.com
+ * License:             GPLv3 or later
+ * License URI:         http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:         redux-framework
+ * Provides:            ReduxFramework
  *
- * @package         ReduxFramework
- * @author          Redux.io by Dovy Paukstys <dovy@redux.io>
- * @license         GNU General Public License, version 3
- * @copyright       2012-2020 Redux.io
+ * @package             ReduxFramework
+ * @author              Extendify
+ * @license             GNU General Public License, version 3
+ * @copyright           2012-2021 Redux.io
  */
 
 // Exit if accessed directly.
