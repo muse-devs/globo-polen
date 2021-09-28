8a37462245b268f5523dfbc38c0bfb96b697e32b
Glaydson Rodrigues <glaydson.queiroz@polen.me>
2021-09-28 16:36:21 -0300

    Start script de autocomplete posteriormente

--- wordpress/masterclass/wp-content/themes/2021_beaba_do_vinho/assets/js/develop.js
+++ wordpress/masterclass/wp-content/themes/2021_beaba_do_vinho/assets/js/develop.js
@@ -71,33 +71,4 @@ jQuery(document).ready(function($) {
         $('#billing_state_field').hide();
     }
 
-    function clean_form()
-    {
-        $('#street').val('');
-        $('#neighborhood').val('');
-        $('#city').val('');
-        $('#state').find('option:selected').remove();
-    }
-
-    $('#billing_postcode').blur(function() {
-        let postcode = $(this).val().replace(/\D/g, '');
-        let check = /^[0-9]{8}$/;
-
-        if (check.test(postcode)) {
-            $('#billing_address_1').val('...');
-            $('#billing_neighborhood').val('...');
-            $('#billing_city').val('...');
-
-            $.getJSON(`https://viacep.com.br/ws/${postcode}/json/?callback=?`, function(data) {
-                if (!('erro' in data)) {
-                    $('#billing_address_1').val(data.logradouro);
-                    $('#billing_neighborhood').val(data.bairro);
-                    $('#billing_city').val(data.localidade);
-                    $('#billing_state').val(data.uf).change();
-                } else {
-                    clean_form();
-                }
-            });
-        }
-    });
 });
