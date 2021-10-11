74074ecab98b0d4abe619882647785d50a5d1105
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2021-10-11 13:18:11 -0300

    update: bootstrap carregado de nodemodules

--- wordpress/polen/themes/polen/functions.php
+++ wordpress/polen/themes/polen/functions.php
@@ -244,7 +244,7 @@ function polen_scripts() {
 		wp_enqueue_script( 'polen-checkout', TEMPLATE_URI . '/assets/js/' . $min . 'checkout.js', array("jquery"), _S_VERSION, true );
 	}
 
-	wp_enqueue_script( 'bootstrap-js', TEMPLATE_URI . '/assets/bootstrap-4.6.0/dist/js/bootstrap.min.js', array("jquery"), _S_VERSION, true );
+	wp_enqueue_script( 'bootstrap-js', TEMPLATE_URI . '/assets/js/min/bootstrap.min.js', array("jquery"), _S_VERSION, true );
 
 	// if(is_user_logged_in()) {
 		wp_enqueue_script( 'header-scripts', TEMPLATE_URI . '/assets/js/' . $min . 'navigation.js', array("jquery"), _S_VERSION, true );
