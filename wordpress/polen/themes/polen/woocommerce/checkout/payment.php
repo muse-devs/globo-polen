d99066f804fbdc8ef2690a225b5e550f970e5ed9
Ian Silva <iansilva6@yahoo.com.br>
2021-10-21 17:23:09 -0300

    feat: estilizando tela do pix

--- wordpress/polen/themes/polen/woocommerce/checkout/payment.php
+++ wordpress/polen/themes/polen/woocommerce/checkout/payment.php
@@ -50,12 +50,12 @@ if ( ! is_ajax() ) {
 
 		<?php woocommerce_checkout_coupon_form(); ?>
 
-		<?php 
+		<?php
 			$button = '<button type="submit" class="btn btn-primary btn-lg btn-block mt-3" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>';
 			if (social_cart_is_social()) {
 				$button = '<button type="submit" class="criesp btn btn-success btn-lg btn-block mt-3" name="woocommerce_checkout_place_order" id="place_order" value="Finalizar doação" data-value="Finalizar doação">Finalizar doação</button>';
 			}
-			echo apply_filters( 'woocommerce_order_button_html', $button); // @codingStandardsIgnoreLine 
+			echo apply_filters( 'woocommerce_order_button_html', $button); // @codingStandardsIgnoreLine
 		?>
 
 		<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
