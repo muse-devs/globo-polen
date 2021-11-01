4f14ba01e64c83c896088d87c7c02d723f612fc1
Rodolfo Neto <rodolfoneto>
2021-11-01 10:38:52 -0300

    feat add: adcionando script do tuna na abertura do body

--- wordpress/polen/themes/polen/inc/analitics_init_body.php
+++ wordpress/polen/themes/polen/inc/analitics_init_body.php
@@ -1,6 +1,6 @@
 <?php
 if ( DEVELOPER ) {
-	return;
+	// return;
 }
 global $Polen_Plugin_Settings;
 
@@ -16,3 +16,32 @@ if( !empty( $Polen_Plugin_Settings['polen_google_tagmanager_key'] ) ) :
 if( !empty( $Polen_Plugin_Settings['polen_ca_pub_key'] ) ) : ?>
 	<script data-ad-client="<?= $Polen_Plugin_Settings['polen_ca_pub_key']; ?>" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <?php endif;
+
+?>
+<script type="text/javascript">
+   if(!session_id()) {
+        session_start();
+	} 
+	<?php $current_user = wp_get_current_user(); ?>
+  var _user_id = "<?php echo !empty( $current_user ) ? $current_user->user_email : ''; ?>";
+  var _session_id = "<?php echo wp_create_nonce(); ?>";
+
+  var _sift = window._sift = window._sift || [];
+  _sift.push(['_setAccount', '57a5a2769e']);
+  _sift.push(['_setUserId', _user_id]);
+  _sift.push(['_setSessionId', _session_id]);
+  _sift.push(['_trackPageview']);
+
+ (function() {
+   function ls() {
+     var e = document.createElement('script');
+     e.src = 'https://cdn.sift.com/s.js';
+     document.body.appendChild(e);
+   }
+   if (window.attachEvent) {
+     window.attachEvent('onload', ls);
+   } else {
+     window.addEventListener('load', ls, false);
+   }
+ })();
+</script>
