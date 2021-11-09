eb637273baca1df66ce3adcb8cbef51fbd9a8546
Rodolfo Neto <rodolfoneto>
2021-11-09 19:06:55 -0300

    no message

---
+++ wordpress/polen/plugins/polen_2/includes/api/Polen_Api_Video_Info.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Polen\Includes\API;
+
+use Polen\Includes\Polen_Video_Info;
+use WP_Error;
+use WP_REST_Request;
+use WP_REST_Server;
+
+class Polen_Api_Video_Info
+{
+    private $namespace;
+    private $resource_name;
+
+    public function __construct()
+    {
+        $this->namespace = '/polen-api/v1';
+        $this->resource_name = 'video-infos';
+    }
+    public function register_routes()
+    {
+        register_rest_route( $this->namespace, '/' . $this->resource_name, array(
+            array(
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => array( $this, 'get_items' ),
+                'permission_callback' => array( $this, 'get_items_permissions_check' )
+            ),
+            'schema' => array( $this, 'get_item_schema' )
+        ) );
+
+        register_rest_route( $this->namespace, '/' . $this->resource_name . '/order/(?P<id>[\d]+)', array(
+            array(
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => array( $this, 'get_item_by_order' ),
+                'permission_callback' => array( $this, 'get_items_permissions_check' )
+            ),
+            'schema' => array( $this, 'get_item_schema' )
+        ) );
+    }
+
+    public function get_items_permissions_check( $request )
+    {
+        if( !current_user_can( 'read' ) ) {
+            return new WP_Error( 'rest_forbidden', 'Você não tem acesso a esse endpoint', array( 'status' => $this->authorization_status_code() ) );
+        }
+        return true;
+    }
+
+
+    /**
+     * 
+     * @param \WP_REST_Request
+     */
+    public function get_items( $request )
+    {
+        $videos_infos = Polen_Video_Info::select_all_videos_incompleted();
+        
+        $data = array();
+
+        if( empty( $videos_infos ) ) {
+            return rest_ensure_response( $videos_infos );
+        }
+
+        foreach( $videos_infos as $video_info ) {
+            $response = $this->prepare_item_for_response( $video_info, $request );
+            $data[] = $response;
+        }
+        
+        return rest_ensure_response( $data );
+    }
+
+    /**
+     * 
+     * @param \WP_REST_Request
+     */
+    public function get_item_by_order( $request )
+    {
+        $order_id = (int) $request['id'];
+        $video_info = Polen_Video_Info::get_by_order_id( $order_id );
+
+        if( empty( $video_info ) ) {
+            return rest_ensure_response( $video_info );
+        }
+        
+        $data = $this->prepare_item_for_response( $video_info, $request );
+
+        return rest_ensure_response( $data );
+    }
+
+    /**
+     * 
+     * @param Polen\Includes\Polen_Video_Info
+     */
+    public function prepare_item_for_response( $video_info, $request )
+    {
+        $post_data = array();
+
+        $post_data[ 'ID' ] = $video_info->ID;
+        $post_data[ 'order_id' ] = $video_info->order_id;
+        $post_data[ 'talent_id' ] = $video_info->talent_id;
+        $post_data[ 'is_public' ] = $video_info->is_public;
+        $post_data[ 'vimeo_id' ] = $video_info->vimeo_id;
+        $post_data[ 'hash' ] = $video_info->hash;
+        $post_data[ 'vimeo_thumbnail' ] = $video_info->vimeo_thumbnail;
+        $post_data[ 'vimeo_process_complete' ] = $video_info->vimeo_process_complete;
+        $post_data[ 'vimeo_url_download' ] = $video_info->vimeo_url_download;
+        $post_data[ 'vimeo_link' ] = $video_info->vimeo_link;
+        $post_data[ 'vimeo_file_play' ] = $video_info->vimeo_file_play;
+        $post_data[ 'duration' ] = $video_info->duration;
+        $post_data[ 'vimeo_iframe' ] = $video_info->vimeo_iframe;
+        $post_data[ 'first_order' ] = $video_info->first_order;
+        $post_data[ 'created_at' ] = $video_info->created_at;
+        $post_data[ 'updated_at' ] = $video_info->updated_at;
+
+        return $post_data;
+    }
+
+    // Sets up the proper HTTP status code for authorization.
+    public function authorization_status_code() {
+
+        $status = 401;
+    
+        if ( is_user_logged_in() ) {
+            $status = 403;
+        }
+    
+        return $status;
+    }
+
+    /**
+     * 
+     */
+    public function get_item_schema()
+    {
+        if( $this->schema ) {
+            return $this->schema;
+        }
+
+        $this->schema = array(
+            '$schema' => 'http://json-schema.org/draft-04/schema#',
+            'title' => 'Video info',
+            'type' => 'object',
+            'properties' => array(
+                'ID' => array(
+                    'description' => 'Id unico do video info',
+                    'type'        => 'integer',
+                    'context'     => array( 'view', 'edit', 'embed' ),
+                    'readonly'    => true,
+                ),
+                'order_id' => array(
+                    'description' => 'ID da Order do video-info',
+                    'type' => 'integer'
+                )
+            )
+        );
+        
+        return $this->schema;
+    }
+}
