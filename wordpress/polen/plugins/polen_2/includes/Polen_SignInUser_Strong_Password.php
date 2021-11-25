3aa683a8e49fca5567b8a3d16536aad5685dda8e
Rodolfo Neto <rodolfoneto>
2021-11-25 09:11:49 -0300

    feat add: cadastrando usuário via API

--- wordpress/polen/plugins/polen_2/includes/Polen_SignInUser_Strong_Password.php
+++ wordpress/polen/plugins/polen_2/includes/Polen_SignInUser_Strong_Password.php
@@ -25,6 +25,12 @@ class Polen_SignInUser_Strong_Password
     }
 
 
+    public function get_password_lenght()
+    {
+        return 6;
+    }
+
+
     /**
      * Valida se a senha é forte
      * @param WP_Error
@@ -37,19 +43,36 @@ class Polen_SignInUser_Strong_Password
         $password = filter_input( INPUT_POST, 'password' );
         $http_referer = filter_input( INPUT_POST, '_wp_http_referer' );
 
+        $password_lenght = $this->get_password_lenght();
+        // Validate password strength
+        $result_strong_password = $this->verify_strong_password( $password );
+        if( '/register/' == $http_referer ) {
+            if( !$result_strong_password ) {
+                // $errors->add( 'registration-error', "A senha deveria ter ao menos {$password_lenght} caracteres, 1 letra maiuscula, 1 numero e 1 caracter especial", 'woocommerce' );//Antiga mensagem quando ainda precisava de uppercase, lowercase e specialChars
+                $errors->add( 'registration-error', $this->get_default_message_error(), 'woocommerce' );
+            }
+        }
+
+        return $errors;
+    }
+
+    public function get_default_message_error()
+    {
+        $password_lenght = $this->get_password_lenght();
+        return "A senha deveria ter ao menos {$password_lenght} caracteres e 1 numero";
+    }
+
+    public function verify_strong_password( $password )
+    {
         $password_lenght = 6;
         // Validate password strength
         $uppercase = true;//preg_match('@[A-Z]@', $password);
         $lowercase = true;//preg_match('@[a-z]@', $password);
         $number    = preg_match('@[0-9]@', $password);
         $specialChars = true;//preg_match('@[^\w]@', $password);
-        if( '/register/' == $http_referer ) {
-            if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < $password_lenght) {
-                // $errors->add( 'registration-error', "A senha deveria ter ao menos {$password_lenght} caracteres, 1 letra maiuscula, 1 numero e 1 caracter especial", 'woocommerce' );//Antiga mensagem quando ainda precisava de uppercase, lowercase e specialChars
-                $errors->add( 'registration-error', "A senha deveria ter ao menos {$password_lenght} caracteres e 1 numero", 'woocommerce' );
-            }
+        if( !$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < $password_lenght ) {
+            return false;
         }
-
-        return $errors;
+        return true;
     }
 }
