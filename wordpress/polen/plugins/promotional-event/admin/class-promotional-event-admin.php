482649401879c3805a399e64522e03a01a8b3960
Rodolfo Neto <rodolfoneto>
2021-11-28 13:30:43 -0300

    feat add: funcao para saber de um produto é evnvtr

--- wordpress/polen/plugins/promotional-event/admin/class-promotional-event-admin.php
+++ wordpress/polen/plugins/promotional-event/admin/class-promotional-event-admin.php
@@ -46,6 +46,7 @@ class Promotional_Event_Admin
 	private $version;
 
 	const ORDER_METAKEY = 'promotional_event';
+	const PRODUCT_METAKEY = '_promotional_event';
     // const SESSION_KEY_CUPOM_CODE = 'event_promotion_cupom_code';
     // const SESSION_KEY_SUCCESS_ORDER_ID = 'event_promotion_order_id';
     const NONCE_ACTION = 'promotional_event_2hj3g42jhg43';
