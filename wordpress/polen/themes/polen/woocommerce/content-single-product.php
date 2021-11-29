0c3cec9ae7b9a8e921eb0ef4e44b7f93053d3504
Rodolfo Neto <rodolfoneto>
2021-11-29 17:52:41 -0300

    ---

--- wordpress/polen/themes/polen/woocommerce/content-single-product.php
+++ wordpress/polen/themes/polen/woocommerce/content-single-product.php
@@ -16,7 +16,7 @@
  * @version 3.6.0
  */
 
-use Polen\Includes\Module\Polen_Product_Module;
+use Polen\Includes\Module\Polen_Product_Module; 
 use Polen\Includes\Polen_Product_B2B;
 
 defined('ABSPATH') || exit;
