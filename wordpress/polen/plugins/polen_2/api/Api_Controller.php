d18631c6d2ee88ca50fc4440850e71f2204e579d
Rodolfo Neto <rodolfoneto>
2021-12-09 18:56:53 -0300

    --

--- wordpress/polen/plugins/polen_2/api/Api_Controller.php
+++ wordpress/polen/plugins/polen_2/api/Api_Controller.php
@@ -391,6 +391,9 @@ class Api_Controller{
 
 
 
+    /**
+     * Gera um nonce para o checkout nao ficar 100% expostos
+     */
     public function create_nonce( \WP_REST_Request $request )
     {
         return api_response( wp_create_nonce( $_SERVER['HTTP_USER_AGENT'].$_SERVER['REMOTE_ADDR'] ) );
