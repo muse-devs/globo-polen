936ab6debbe89865914e30d8a90790800b41a75f
Glaydson Rodrigues <glaydson.queiroz@polen.me>
2022-01-04 17:14:47 -0300

    Exibir origem do pedido

--- wordpress/polen/themes/polen/api/api_function.php
+++ wordpress/polen/themes/polen/api/api_function.php
@@ -11,3 +11,17 @@ function api_response($data = null, int $status_code = 200, array $headers = [])
 {
   return new WP_REST_Response(['data' => $data], $status_code, $headers);
 }
+
+/**
+ * Exibir origem do pedido
+ */
+function custom_fields_in_order_admin($order)
+{
+    $origin = 'polen_site';
+    if (get_post_meta($order->id, 'hotsite', true)) {
+        $origin = get_post_meta($order->id, 'hotsite', true);
+    }
+
+    echo "<p><strong>Origem:</strong><br> {$origin} </p><br>";
+}
+add_action('woocommerce_admin_order_data_after_billing_address', 'custom_fields_in_order_admin', 10, 1);
