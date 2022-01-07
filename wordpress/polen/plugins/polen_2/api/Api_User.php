c0fc45cf1e5b8ebbc7397f45ec6ffc405e240171
Glaydson Rodrigues <glaydson.queiroz@polen.me>
2022-01-07 16:34:07 -0300

    Adicionar atributo para nome completo

--- wordpress/polen/plugins/polen_2/api/Api_User.php
+++ wordpress/polen/plugins/polen_2/api/Api_User.php
@@ -134,6 +134,7 @@ class Api_User
 
         $response = [
             'ID' => $user->data->ID,
+            'name' => get_user_meta($user->data->ID,'first_name', true) . ' ' . get_user_meta($user->data->ID,'last_name', true),
             'first_name' => get_user_meta($user->data->ID,'first_name', true),
             'last_name' => get_user_meta($user->data->ID,'last_name', true),
             'phone' => get_user_meta($user->data->ID,'billing_phone', true),
