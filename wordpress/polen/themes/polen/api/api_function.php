d6cdbec61524554817983f42db9c6d043b3a1c3c
raorodrigues <raorodrigues@gmail.com>
2022-01-07 16:53:42 -0300

    Homolog (#2189)

--- wordpress/polen/themes/polen/api/api_function.php
+++ wordpress/polen/themes/polen/api/api_function.php
@@ -22,6 +22,11 @@ function custom_fields_in_order_admin($order)
         $origin = get_post_meta($order->id, 'hotsite', true);
     }
 
+    $order_is_ep = event_promotional_order_is_event_promotional($order);
+    if ($order_is_ep) {
+        $origin = event_promotional_order_get_slug_event($order);
+    }
+
     echo "<p><strong>Origem:</strong><br> {$origin} </p><br>";
 }
 add_action('woocommerce_admin_order_data_after_billing_address', 'custom_fields_in_order_admin', 10, 1);
