55795d21bce9fd4d55713a706d4f3c010409453d
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2022-02-14 16:01:57 -0300

    feat: base para novo Polen Empresas

--- wordpress/polen/themes/polen/b2b/src/components/index.ts
+++ wordpress/polen/themes/polen/b2b/src/components/index.ts
@@ -1,1 +1,1 @@
-// export { default as PolAvatar } from "src/components/PolAvatar";
+export { default as PolSEO } from "components/PolSEO";
--- wordpress/polen/themes/polen/b2b/src/pages/index.tsx
+++ wordpress/polen/themes/polen/b2b/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import { PolSEO } from "components";
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Polen B2B</title>
-      <h1>Polen Para Empresas</h1>
-    </main>
+    <>
+      <PolSEO />
+      <main>
+        <h1>Polen Para Empresas</h1>
+      </main>
+    </>
   );
 };
 
