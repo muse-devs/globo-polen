fba3f2fbd60a2bea32927e625fbf87623f702c9c
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2022-02-15 16:52:18 -0300

    vou dar pull

--- wordpress/polen/themes/polen/b2b/src/components/PolB2BSuperBanner/index.tsx
+++ wordpress/polen/themes/polen/b2b/src/components/PolB2BSuperBanner/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Row, Col, Button } from "react-bootstrap";
+import { PolScrollable } from "components";
+import kovi from "images/logos/kovi.png";
+import "./styles.scss";
 
 const videos = [
   {
-    image: "",
-    video: "",
-    logo: "",
-    name: "",
+    image:
+      "https://i.vimeocdn.com/video/1364305989-55d3b1bef407347f5c2c527cf9db5b00bf9e80daf4d5dfe26081140cb47999ad-d",
+    video:
+      "https://player.vimeo.com/progressive_redirect/playback/634757715/rendition/720p?loc=external&oauth2_token_id=1511985459&signature=11f36c28ddac629de6b3f726d073bca4c9a4a3847530345c6eb53258bfdbaaf6",
+    logo: kovi,
+    name: "item1",
   },
   {
-    image: "",
-    video: "",
-    logo: "",
-    name: "",
+    image:
+      "https://i.vimeocdn.com/video/1364305989-55d3b1bef407347f5c2c527cf9db5b00bf9e80daf4d5dfe26081140cb47999ad-d",
+    video:
+      "https://player.vimeo.com/progressive_redirect/playback/634757715/rendition/720p?loc=external&oauth2_token_id=1511985459&signature=11f36c28ddac629de6b3f726d073bca4c9a4a3847530345c6eb53258bfdbaaf6",
+    logo: kovi,
+    name: "item2",
   },
 ];
 
@@ -20,7 +27,7 @@ export default function () {
   return (
     <section>
       <Row>
-        <Col md={6} className="ps-4">
+        <Col md={6} className="ps-4 order-1 order-md-0">
           <h1 className="typo-xl">
             Use os vídeos personalizados dos ídolos da Polen para impulsionar o
             seu <em>negócio</em>
@@ -32,23 +39,26 @@ export default function () {
           <Button href="#faleconosco">Fale com a equipe de vendas</Button>
         </Col>
         <Col md={6}>
-          <section>
-            <div>
-              {videos.map((item) => (
-                <div className="item">
+          <PolScrollable id={"super-banner"}>
+            {videos.map((item) => (
+              <section>
+                <div className="super-banner-item me-4" key={item.name}>
                   <figure className="video-card">
-                    <img src="" alt="" className="poster" />
-                    <video src="" className="video-player"></video>
+                    <img src={item.image} alt={item.name} className="poster" />
+                    <video
+                      src={item.video}
+                      className="video-player"
+                      playsInline
+                    ></video>
                   </figure>
                   <figure className="logo">
-                    <img src="" alt="" className="image" />
-                    <figcaption className="name">Marca</figcaption>
+                    <img src={item.logo} alt={item.name} className="image" />
+                    <figcaption className="name">{item.name}</figcaption>
                   </figure>
                 </div>
-              ))}
-            </div>
-            <div>nav</div>
-          </section>
+              </section>
+            ))}
+          </PolScrollable>
         </Col>
       </Row>
     </section>
