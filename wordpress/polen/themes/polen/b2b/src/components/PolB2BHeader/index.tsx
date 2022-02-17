8c0d59b1a20be9407b5f36c1c1d66f8a2b429ba9
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2022-02-17 09:56:55 -0300

    ajuste no grid

--- wordpress/polen/themes/polen/b2b/src/components/PolB2BHeader/index.tsx
+++ wordpress/polen/themes/polen/b2b/src/components/PolB2BHeader/index.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { ChevronLeft } from "react-feather";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
 import "./styles.scss";
 import logo from "images/logo-b2b.png";
 
 const PolB2bHeader = () => {
   return (
-    <Container>
-      <Row className="py-4">
-        <Col xs={6} className="d-flex align-items-center">
-          <a href="/">
-            <ChevronLeft />
-            <img src={logo} alt="Logo B2B" width={114} />
-          </a>
-        </Col>
-        <Col xs={6} className="d-flex align-items-center justify-content-end">
-          <Button variant="outline-light" size="sm" href="#faleconosco">
-            Fale Conosco
-          </Button>
-        </Col>
-      </Row>
-    </Container>
+    <Row className="py-4 px-md-5">
+      <Col xs={6} className="d-flex align-items-center">
+        <a href="/">
+          <ChevronLeft />
+          <img src={logo} alt="Logo B2B" width={114} />
+        </a>
+      </Col>
+      <Col xs={6} className="d-flex align-items-center justify-content-end">
+        <Button variant="outline-light" size="sm" href="#faleconosco">
+          Fale Conosco
+        </Button>
+      </Col>
+    </Row>
   );
 };
 
