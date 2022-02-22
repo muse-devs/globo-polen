3c84b9325fc9229908ea6d074bed4cd691b03a62
raorodrigues <raorodrigues@gmail.com>
2022-02-22 12:05:13 -0300

    fix: url redirecionando errado (#2376)

---
+++ wordpress/polen/themes/polen/b2b/gatsby-node.js
@@ -0,0 +1,20 @@
+const path = require("path")
+const fs = require("fs")
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "empresas"), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "empresas"))
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
+}
