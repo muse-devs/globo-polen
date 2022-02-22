da80196dbc5b4c432cda3b57ed39cb20398db161
Ricardo Rodrigues <ricardo.rodrigues@polen.me>
2022-02-22 18:04:12 -0300

    ajustes

--- wordpress/polen/themes/polen/woocommerce/content-single-product.php
+++ wordpress/polen/themes/polen/woocommerce/content-single-product.php
@@ -25,11 +25,6 @@ global $Polen_Plugin_Settings;
 global $post;
 
 $polen_product = new Polen_Product_Module( $product );
-// if( $polen_product->get_is_campaign() ) {
-// 	$campaign_slug = $polen_product->get_campaign_slug();
-// 	wc_get_template( "content-single-{$campaign_slug}-product.php" );
-// 	return;
-// }
 
 /**
  * Hook: woocommerce_before_single_product.
@@ -60,9 +55,7 @@ $donate = get_post_meta(get_the_ID(), '_is_charity', true);
 $donate_name = get_post_meta(get_the_ID(), '_charity_name', true);
 $donate_image =  get_post_meta(get_the_ID(), '_url_charity_logo', true);
 $donate_text = stripslashes(get_post_meta(get_the_ID(), '_description_charity', true));
-// $social = social_product_is_social($product, social_get_category_base()); //Antigo CRIESP
 
-$histories_enabled = $Polen_Plugin_Settings['polen_histories_on'];
 $social = Social_Base_Product::product_is_social_base( $product );
 $inputs = new Material_Inputs();
 
@@ -78,26 +71,8 @@ if( 'instock' == $product->get_stock_status() ) {
 } else {
 	$has_stock = false;
 }
-// $stock = $product->get_stock_status();
 
 ?>
-
-<?php if($histories_enabled) : ?>
-  <script>
-    // params
-    jQuery(document).ready(function() {
-      if(document.querySelector("#stories")) {
-        renderStories(
-          <?php echo polen_get_videos_by_talent($Talent_Fields, true); ?>,
-          <?php echo json_encode(get_the_title()); ?>,
-          <?php echo json_encode($image_data["image"]); ?>,
-          <?php echo $social || 'null'; ?>,
-          <?php echo $stock || 'null'; ?>
-          );
-      }
-    });
-  </script>
-<?php endif; ?>
 
 <?php if ($bg_image) : ?>
 	<figure class="image-bg">
@@ -109,8 +84,7 @@ if( 'instock' == $product->get_stock_status() ) {
 
 	<!-- Tags -->
 	<div class="row">
-		<div class="col-12<?php echo $histories_enabled ? ' col-md-6 m-md-auto' : ''; ?> d-flex align-items-center">
-			<?php $histories_enabled && polen_front_get_talent_stories(); ?>
+		<div class="col-12 d-flex align-items-center">
 			<?php if(sizeof($videos) > 0) : ?>
 				<div>
 					<h1 class="talent-name" title="<?= get_the_title(); ?>"><?= get_the_title(); ?></h1>
@@ -118,7 +92,6 @@ if( 'instock' == $product->get_stock_status() ) {
 			<?php endif; ?>
 			<?php polen_get_share_button(); ?>
 		</div>
-		<?php if(!$histories_enabled) : ?>
 			<?php if($videos && sizeof($videos) > 0): ?>
 				<div class="col-12 mt-3">
 					<?php polen_front_get_videos_single($Talent_Fields, $videos, $image_data); ?>
@@ -128,7 +101,6 @@ if( 'instock' == $product->get_stock_status() ) {
 					<?php polen_front_get_talent_mini_bio($image_data, get_the_title(), $categories[0]->name); ?>
 				</div>
 			<?php endif; ?>
-		<?php endif; ?>
 	</div>
 
   <!-- Botão de adicionar ao carrinho -->
@@ -139,7 +111,7 @@ if( 'instock' == $product->get_stock_status() ) {
         "select_type",
         "select_type",
         array(
-          $inputs->pol_combo_advanced_item("Vídeo para uso pessoal", $product->get_price_html(), "Compre um vídeo personalizado para você ou para presentar outra pessoa", "check-pessoal", "pessoal", true),
+          $inputs->pol_combo_advanced_item("Vídeo para uso pessoal", $product->get_price_html(), "Compre um vídeo personalizado para você ou para presentar outra pessoa", "check-pessoal", "pessoal", true, false),
           $inputs->pol_combo_advanced_item("Vídeo para meu negócio", "Valor sob consulta", "Compre um Vídeo Polen para usar no seu negócio", "check-b2b", "b2b", false, !polen_b2b_product_is_enabled($product))
           )); ?>
 				<div class="btn-buy-personal">
@@ -181,24 +153,12 @@ if( 'instock' == $product->get_stock_status() ) {
 					</div>
 				</div>
 			<?php endif; ?>
-			<?php /* if ($social) : ? >
-				<div class="row">
-					<div class="col-md-12 mb-1">
-						<?php polen_donate_badge("100% DO VALOR DOADO PARA O CRIANÇA ESPERANÇA", false, true); ?>
-					</div>
-				</div>
-			<?php endif; */?>
-			<!-- /------------ -->
 		</div>
 	</div>
 
 	<!-- Card dos Reviews -->
-	<?php //$social || polen_card_talent_reviews_order($post, $Talent_Fields); ?>
   <?php $social || polen_talent_deadline($post, $Talent_Fields); ?>
 
-	<?php
-		// if (!$social) {
-	?>
 		<div class="row mt-4">
 			<div class="col-12 col-md-6 m-md-auto">
 				<?php if (count($terms) > 0) : ?>
@@ -208,11 +168,6 @@ if( 'instock' == $product->get_stock_status() ) {
 				<?php endif; ?>
 			</div>
 		</div>
-	<?php
-		// } else {
-			//  criesp_get_send_video_date();
-		// }
-	?>
 
 	<!-- Bio -->
 	<div class="row mt-4">
@@ -232,19 +187,6 @@ if( 'instock' == $product->get_stock_status() ) {
 	$video_depoimento = $product->get_meta( Social_Base_Product::PRODUCT_META_VIDEO_TESTEMONIAL_URL, true );
 	$social && sa_get_about($video_depoimento);
 	?>
-
-  <?php
-  // Campanha Luccas Neto dia das crianças ----
-  $is_luccas_neto = $Polen_Plugin_Settings['promotional-event-luccas-neto'] == get_the_ID();
-  if($is_luccas_neto) {
-    generic_get_about(
-      "Campanha dia das Crianças",
-      "Dia das Crianças com Luccas Neto",
-      "<p>A Polen e o Luccas Neto vão escolher as quatro histórias e mensagens mais emocionantes para presentear com o boneco autografado do Luccas, entre os pedidos realizados e confirmados até o dia 5 de Outubro!</p>
-      <p>Capriche na mensagem de Dia das Crianças do seu vídeo-Polen! Com certeza quem você presentear vai se emocionar e ainda terá a chance de receber mais um super presente para acompanhar o recado exclusivo que o Luccas vai gravar.</p>"
-    );
-  }
-  ?>
 
   <!-- Avaliações -->
 	<?php $social || polen_talent_review($reviews); ?>
@@ -271,21 +213,6 @@ foreach ($array_sites as $key => $site) {
 }
 
 $logo_dark = wp_get_attachment_image_url( get_theme_mod( 'custom_logo' ), 'full' );
-
-// pol_print_schema_data(array(
-// 	"url" 						=> $Talent_Fields->talent_url,
-// 	"title" 					=> get_the_title(),
-// 	"image" 					=> $bg_image,
-// 	"date_published"			=> $Talent_Fields->created,
-// 	"date_modified" 			=> $Talent_Fields->updated,
-// 	"date_created" 				=> $Talent_Fields->created,
-// 	"talent_name" 				=> $Talent_Fields->nome,
-// 	"talent_url" 				=> $Talent_Fields->talent_url,
-// 	"talent_image" 				=> polen_get_avatar_src($Talent_Fields->user_id, 'polen-square-crop-lg'),
-// 	"talent_social_links_array"	=> $array_social,
-// 	"logo" 						=> $logo_dark,
-// 	"description" 				=> $Talent_Fields->descricao
-// ));
 
 // Novo conteúdo Schema.org
 $total_reviews = Polen_Order_Review::get_number_total_reviews_by_talent_id($talent->ID);
